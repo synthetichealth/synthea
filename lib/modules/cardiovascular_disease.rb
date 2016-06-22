06d76434be577ea0d718e60abfdb329c258ef35b
Maurice Wong <mwong@mitre.org>
2016-06-22 09:30:29 -0400

    fixed additional tab issues

--- lib/modules/cardiovascular_disease.rb
+++ lib/modules/cardiovascular_disease.rb
@@ -186,21 +186,21 @@ module Synthea
 
       #chance of getting a sudden cardiac arrest without heart disease. (Most probable cardiac event w/o cause or history)
       rule :no_coronary_heart_disease, [:coronary_heart_disease?], [:cardiac_arrest, :death] do |time, entity|
-          annual_risk = Synthea::Config.cardiovascular.sudden_cardiac_arrest.risk
-          cardiac_event_chance = Synthea::Rules.convert_risk_to_timestep(annual_risk,365)
-          if entity[:coronary_heart_disease].nil? && rand < cardiac_event_chance
-              entity.events.create(time, :cardiac_arrest, :no_coronary_heart_disease)
-              entity.events.create(time, :emergency_encounter, :no_coronary_heart_disease)
-              Synthea::Modules::Encounters.emergency_visit(time, entity)
-              survival_rate = 1 - Synthea::Config.cardiovascular.sudden_cardiac_arrest.death
-              survival_rate *= 3 if rand < Synthea::Config.cardiovascular.chd.bystander
-              annual_death_risk = 1 - survival_rate
-              if rand < Synthea::Rules.convert_risk_to_timestep(annual_death_risk,365)
-                  entity[:is_alive] = false
-                  entity.events.create(time, :death, :no_coronary_heart_disease, true)
-                  Synthea::Modules::Lifecycle::Record.death(entity, time)
-              end
+        annual_risk = Synthea::Config.cardiovascular.sudden_cardiac_arrest.risk
+        cardiac_event_chance = Synthea::Rules.convert_risk_to_timestep(annual_risk,365)
+        if entity[:coronary_heart_disease].nil? && rand < cardiac_event_chance
+          entity.events.create(time, :cardiac_arrest, :no_coronary_heart_disease)
+          entity.events.create(time, :emergency_encounter, :no_coronary_heart_disease)
+          Synthea::Modules::Encounters.emergency_visit(time, entity)
+          survival_rate = 1 - Synthea::Config.cardiovascular.sudden_cardiac_arrest.death
+          survival_rate *= 3 if rand < Synthea::Config.cardiovascular.chd.bystander
+          annual_death_risk = 1 - survival_rate
+          if rand < Synthea::Rules.convert_risk_to_timestep(annual_death_risk,365)
+            entity[:is_alive] = false
+            entity.events.create(time, :death, :no_coronary_heart_disease, true)
+            Synthea::Modules::Lifecycle::Record.death(entity, time)
           end
+        end
       end
 
       #-----------------------------------------------------------------------#
@@ -212,41 +212,41 @@ module Synthea
 
       #data for men is first array, women in second.
       age_stroke = [
-          [(54..56), (57..59), (60..62), (63..65), (66..68), (69..72),
-          (73..75), (76..78), (79..81), (82..84), (85..999)],
+        [(54..56), (57..59), (60..62), (63..65), (66..68), (69..72),
+        (73..75), (76..78), (79..81), (82..84), (85..999)],
 
-          [(54..56), (57..59), (60..62), (63..64), (65..67), (68..70),
-          (71..73), (74..76), (77..78), (79..81), (82..999)]
+        [(54..56), (57..59), (60..62), (63..64), (65..67), (68..70),
+        (71..73), (74..76), (77..78), (79..81), (82..999)]
       ]
 
       untreated_sys_bp_stroke = [
-          [(0..105), (106..115), (116..125), (126..135), (136..145), (146..155),
-          (156..165), (166..175), (176..185), (185..195), (196..205)],
+        [(0..105), (106..115), (116..125), (126..135), (136..145), (146..155),
+        (156..165), (166..175), (176..185), (185..195), (196..205)],
 
-          [(0..95), (95..106), (107..118), (119..130), (131..143), (144..155),
-          (156..167), (168..180), (181..192), (193..204), (205..216)]
+        [(0..95), (95..106), (107..118), (119..130), (131..143), (144..155),
+        (156..167), (168..180), (181..192), (193..204), (205..216)]
       ]
 
       treated_sys_bp_stroke = [
-          [(0..105), (106..112), (113..117), (118..123), (124..129), (130..135),
-          (136..142), (143..150), (151..161), (162..176), (177..205)],
+        [(0..105), (106..112), (113..117), (118..123), (124..129), (130..135),
+        (136..142), (143..150), (151..161), (162..176), (177..205)],
 
-          [(0..95), (95..106), (107..113), (114..119), (120..125), (126..131),
-          (132..139), (140..148), (149..160), (161..204), (205..216)]
+        [(0..95), (95..106), (107..113), (114..119), (120..125), (126..131),
+        (132..139), (140..148), (149..160), (161..204), (205..216)]
       ]
 
       ten_year_stroke_risk = {
-          'M' => {
-              0 => 0, 1 => 0.03, 2 => 0.03, 3 => 0.04, 4 => 0.04, 5 => 0.05, 6 => 0.05, 7 => 0.06, 8 => 0.07, 9 =>0.08, 10 => 0.1,
-              11 => 0.11, 12 => 0.13, 13 => 0.15, 14 => 0.17, 15 => 0.2, 16 => 0.22, 17 => 0.26, 18 => 0.29, 19 => 0.33, 20 => 0.37,
-              21 => 0.42, 22 => 0.47, 23 => 0.52, 24 => 0.57, 25 => 0.63, 26 => 0.68, 27 => 0.74, 28 => 0.79, 29 => 0.84, 30 => 0.88
-          },
+        'M' => {
+          0 => 0, 1 => 0.03, 2 => 0.03, 3 => 0.04, 4 => 0.04, 5 => 0.05, 6 => 0.05, 7 => 0.06, 8 => 0.07, 9 =>0.08, 10 => 0.1,
+          11 => 0.11, 12 => 0.13, 13 => 0.15, 14 => 0.17, 15 => 0.2, 16 => 0.22, 17 => 0.26, 18 => 0.29, 19 => 0.33, 20 => 0.37,
+          21 => 0.42, 22 => 0.47, 23 => 0.52, 24 => 0.57, 25 => 0.63, 26 => 0.68, 27 => 0.74, 28 => 0.79, 29 => 0.84, 30 => 0.88
+        },
 
-          'F' => {
-              0 => 0, 1 => 0.01, 2 => 0.01, 3 => 0.02, 4 => 0.02, 5 => 0.02, 6 => 0.03, 7 => 0.04, 8 => 0.04, 9 =>0.05, 10 => 0.06,
-              11 => 0.08, 12 => 0.09, 13 => 0.11, 14 => 0.13, 15 => 0.16, 16 => 0.19, 17 => 0.23, 18 => 0.27, 19 => 0.32, 20 => 0.37,
-              21 => 0.43, 22 => 0.5, 23 => 0.57, 24 => 0.64, 25 => 0.71, 26 => 0.78, 27 => 0.84
-          }
+        'F' => {
+          0 => 0, 1 => 0.01, 2 => 0.01, 3 => 0.02, 4 => 0.02, 5 => 0.02, 6 => 0.03, 7 => 0.04, 8 => 0.04, 9 =>0.05, 10 => 0.06,
+          11 => 0.08, 12 => 0.09, 13 => 0.11, 14 => 0.13, 15 => 0.16, 16 => 0.19, 17 => 0.23, 18 => 0.27, 19 => 0.32, 20 => 0.37,
+          21 => 0.43, 22 => 0.5, 23 => 0.57, 24 => 0.64, 25 => 0.71, 26 => 0.78, 27 => 0.84
+        }
       }
 
       diabetes_stroke = {'M' => 2,'F' => 3}
@@ -254,65 +254,65 @@ module Synthea
       atrial_fibrillation_stroke_points = {'M' => 4, 'F' => 6}
 
       rule :calculate_stroke_risk, [:age, :diabetes, :coronary_heart_disease, :blood_pressure, :stroke_history, :smoker], [:stroke_risk] do |time, entity|
-          return if entity[:age].nil? || entity[:blood_pressure].nil? || entity[:gender].nil? 
-          age = entity[:age]
-          gender = entity[:gender]
-          blood_pressure = entity[:blood_pressure][0]
-          #https://www.heart.org/idc/groups/heart-public/@wcm/@sop/@smd/documents/downloadable/ucm_449858.pdf
-          #calculate stroke risk based off of prevalence of stroke in age group for people younger than 54. Framingham score system does not cover these.
-          
-          if gender == 'M'
-              gender_index = 0
-          else
-              gender_index = 1
-          end
+        return if entity[:age].nil? || entity[:blood_pressure].nil? || entity[:gender].nil? 
+        age = entity[:age]
+        gender = entity[:gender]
+        blood_pressure = entity[:blood_pressure][0]
+        #https://www.heart.org/idc/groups/heart-public/@wcm/@sop/@smd/documents/downloadable/ucm_449858.pdf
+        #calculate stroke risk based off of prevalence of stroke in age group for people younger than 54. Framingham score system does not cover these.
+        
+        if gender == 'M'
+          gender_index = 0
+        else
+          gender_index = 1
+        end
 
-          case
-          when age < 20
-              return
-          when age < 40 && age >= 20
-              rate = Synthea::Config.cardiovascular.stroke.rate_20_39[gender_index]
-          when age < 55 && age >=40
-              rate = Synthea::Config.cardiovascular.stroke.rate_40_59[gender_index]
-          end
+        case
+        when age < 20
+          return
+        when age < 40 && age >= 20
+          rate = Synthea::Config.cardiovascular.stroke.rate_20_39[gender_index]
+        when age < 55 && age >=40
+          rate = Synthea::Config.cardiovascular.stroke.rate_40_59[gender_index]
+        end
 
-          if rate
-              entity[:stroke_risk] = Synthea::Rules.convert_risk_to_timestep(rate, 3650) 
-              return
-          end
+        if rate
+          entity[:stroke_risk] = Synthea::Rules.convert_risk_to_timestep(rate, 3650) 
+          return
+        end
 
-          stroke_points = 0
-          stroke_points += 3 if entity[:smoker]
-          stroke_points += 5 if entity[:left_ventricular_hypertrophy]
-          stroke_points += age_stroke[gender_index].find_index{|range| range.include?(age)}
-          if entity[:bp_treated?] #treating blood pressure currently is not a feature. Modify this for when it is.
-              stroke_points += treated_sys_bp_stroke[gender_index].find_index{|range| range.include?(blood_pressure)}
-          else 
-              stroke_points += untreated_sys_bp_stroke[gender_index].find_index{|range| range.include?(blood_pressure)}
-          end
-          stroke_points += diabetes_stroke[gender] if entity[:diabetes]
-          stroke_points += chd_stroke_points[gender] if entity[:coronary_heart_disease]
-          stroke_points += atrial_fibrillation_stroke_points[gender] if entity[:atrial_fibrillation]
-          ten_stroke_risk = ten_year_stroke_risk[gender][stroke_points]
-          binding.pry if ten_stroke_risk.nil?
+        stroke_points = 0
+        stroke_points += 3 if entity[:smoker]
+        stroke_points += 5 if entity[:left_ventricular_hypertrophy]
+        stroke_points += age_stroke[gender_index].find_index{|range| range.include?(age)}
+        if entity[:bp_treated?] #treating blood pressure currently is not a feature. Modify this for when it is.
+          stroke_points += treated_sys_bp_stroke[gender_index].find_index{|range| range.include?(blood_pressure)}
+        else 
+          stroke_points += untreated_sys_bp_stroke[gender_index].find_index{|range| range.include?(blood_pressure)}
+        end
+        stroke_points += diabetes_stroke[gender] if entity[:diabetes]
+        stroke_points += chd_stroke_points[gender] if entity[:coronary_heart_disease]
+        stroke_points += atrial_fibrillation_stroke_points[gender] if entity[:atrial_fibrillation]
+        ten_stroke_risk = ten_year_stroke_risk[gender][stroke_points]
+        binding.pry if ten_stroke_risk.nil?
 
-          #divide 10 year risk by 365 * 10 to get daily risk.
-          entity[:stroke_risk] = Synthea::Rules.convert_risk_to_timestep(ten_stroke_risk, 3650)
-          entity[:stroke_points] = stroke_points
+        #divide 10 year risk by 365 * 10 to get daily risk.
+        entity[:stroke_risk] = Synthea::Rules.convert_risk_to_timestep(ten_stroke_risk, 3650)
+        entity[:stroke_points] = stroke_points
       end
 
       rule :get_stroke, [:stroke_risk, :stroke_history], [:stroke, :death, :stroke_history] do |time, entity|
-          if entity[:stroke_risk] && rand < entity[:stroke_risk]
-              entity.events.create(time, :stroke, :get_stroke)
-              entity[:stroke_history] = true
-              entity.events.create(time + 10.minutes, :emergency_encounter, :get_stroke)
-              Synthea::Modules::Encounters.emergency_visit(time + 15.minutes, entity)
-              if rand < Synthea::Config.cardiovascular.stroke.death
-                  entity[:is_alive] = false
-                  entity.events.create(time, :death, :get_stroke, true)
-                  Synthea::Modules::Lifecycle::Record.death(entity, time)
-              end
+        if entity[:stroke_risk] && rand < entity[:stroke_risk]
+          entity.events.create(time, :stroke, :get_stroke)
+          entity[:stroke_history] = true
+          entity.events.create(time + 10.minutes, :emergency_encounter, :get_stroke)
+          Synthea::Modules::Encounters.emergency_visit(time + 15.minutes, entity)
+          if rand < Synthea::Config.cardiovascular.stroke.death
+            entity[:is_alive] = false
+            entity.events.create(time, :death, :get_stroke, true)
+            Synthea::Modules::Lifecycle::Record.death(entity, time)
           end
+        end
       end
 
       #-----------------------------------------------------------------------#
