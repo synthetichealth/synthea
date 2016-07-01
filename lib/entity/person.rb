2bf070b4af887fc90b227a18ded80ffa445a06de
Maurice Wong <mwong@mitre.org>
2016-07-01 10:48:59 -0400

    Finish fhir record refactoring. Delete code to write/keep track of ccda

--- lib/entity/person.rb
+++ lib/entity/person.rb
@@ -2,14 +2,10 @@ module Synthea
   class Person < Synthea::Entity
 
   	# TODO: move the record into a separate class that tracks data
-    attr_accessor :record, :record_conditions, :fhir_record ,:record_synthea# Health Data Standards Record
+    attr_accessor :record_synthea# Health Data Standards Record
 
     def initialize
       super
-      @record_conditions={}
-      @record = Record.new
-      @fhir_record = FHIR::Bundle.new
-      @fhir_record.type = 'collection'
       @record_synthea = Synthea::Output::Record.new
     end
 
--- test/unit/immunizations_test.rb
+++ test/unit/immunizations_test.rb
@@ -7,41 +7,24 @@ class ImmunizationsTest < Minitest::Test
     @patient[:age] = 0
     @time = Synthea::Config.start_date
 	  @patient.events.create(@time, :birth, :birth, true)
-    entry = FHIR::Bundle::Entry.new({'fullUrl'=>'123'})
-    entry.resource = FHIR::Patient.new
-  	@patient.fhir_record.entry << entry
-  	entry = FHIR::Bundle::Entry.new('fullUrl'=>'789')
-  	entry.resource = FHIR::Encounter.new
-  	@patient.fhir_record.entry << entry
   end
 
   def test_birth_immunizations
-  	Synthea::Modules::Immunizations::Record.perform_encounter(@patient, @time)
+  	Synthea::Modules::Immunizations.perform_encounter(@patient, @time)
   	assert_equal(1, @patient[:immunizations].length)
   	assert_equal(1, @patient[:immunizations][:hepb].length)
   	assert_equal(@time, @patient[:immunizations][:hepb][0])
-  	immunization_entry = @patient.fhir_record.entry.reverse.find {|e| e.resource.is_a?(FHIR::Immunization)}
-  	immunization = immunization_entry.resource
-  	assert_equal('Patient/123',immunization.patient.reference)
-  	assert_equal('Encounter/789',immunization.encounter.reference)
-  	assert_equal("http://hl7.org/fhir/sid/cvx", immunization.vaccineCode.coding[0].system)
-  	assert_equal("08", immunization.vaccineCode.coding[0].code)
-  	assert_equal("Hep B, adolescent or pediatric", immunization.vaccineCode.coding[0].display)
-    assert_equal(Synthea::Rules::BaseRecord.convertFhirDateTime(@time,'time'),immunization.date)
-    assert_equal('completed', immunization.status)
-  	assert_equal(false, immunization.wasNotGiven)
-  	assert_equal(false, immunization.reported)
   end
 
   def test_two_month_immunizations
   	# birth immunizations
-    	Synthea::Modules::Immunizations::Record.perform_encounter(@patient, @time)
+    Synthea::Modules::Immunizations.perform_encounter(@patient, @time)
   	# 1-month immunizations
   	time_1m = @time.advance(:months => 1)
-  	Synthea::Modules::Immunizations::Record.perform_encounter(@patient, time_1m)
+  	Synthea::Modules::Immunizations.perform_encounter(@patient, time_1m)
   	# 2-month immunizations
   	time_2m = @time.advance(:months => 2)
-  	Synthea::Modules::Immunizations::Record.perform_encounter(@patient, time_2m)
+  	Synthea::Modules::Immunizations.perform_encounter(@patient, time_2m)
 
   	assert_equal(6, @patient[:immunizations].length)
   	assert_equal(2, @patient[:immunizations][:hepb].length)
