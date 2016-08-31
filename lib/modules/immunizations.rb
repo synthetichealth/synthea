95574f20f1b2b722b60579b4fb9b58ed24acebdc
Pete K <krautscheid@mitre.org>
2016-08-31 13:42:19 -0400

    Add support for historical availibility of vaccines

--- lib/modules/immunizations.rb
+++ lib/modules/immunizations.rb
@@ -25,21 +25,53 @@ module Synthea
         birthdate = entity.event(:birth).time
         age_in_months = Synthea::Modules::Lifecycle.age(time, birthdate, nil, :months)
         IMM_SCHEDULE.each_key do |imm|
-          if immunization_due(imm, age_in_months, entity[:immunizations][imm])
+          if immunization_due(imm, birthdate, age_in_months, time, entity[:immunizations][imm] || [])
             entity[:immunizations][imm] ||= []
             entity[:immunizations][imm] << time
+            # puts "Administering #{IMM_SCHEDULE[imm][:code]['display']} to #{entity[:name_first]} age #{age_in_months/12} in year #{time.year}"
             patient.immunization(imm, time, :immunization, :immunization)
           end
         end
       end
 
-      def self.immunization_due(imm,age_in_months,history)
-        history ||= []
+      def self.immunization_due(imm, birthdate, age_in_months, time, history)
+
         at_months = IMM_SCHEDULE[imm][:at_months]
-        if history.length < at_months.length
-          return age_in_months >= at_months[history.length]
+        first_available = IMM_SCHEDULE[imm][:first_available]
+
+        # Don't administer if the immunization wasn't historically available at the date of the encounter
+        return false if first_available && time.year < first_available
+
+        # Don't administer if all recommended doses have already been given
+        return false if history.length >= at_months.length
+
+        # See if the patient should recieve a dose based on their current age and the recommended dose ages;
+        # we can't just see if greater than the recommended age for the next dose they haven't received
+        # because ie we don't want to administer the HPV vaccine to someone who turns 90 in 2006 when the
+        # vaccine is released; we can't just use a simple test of, say, within 4 years after the recommended
+        # age for the next dose they haven't received because ie PCV13 is given to kids and seniors but was
+        # only available starting in 2010, so a senior in 2012 who has never received a dose should get one,
+        # but only one; what we do is
+
+        # 1) eliminate any recommended doses that are not within 4 years of the patient's age
+        at_months = at_months.reject { |am| age_in_months - am >= 48 }
+
+        # 2) eliminate recommended doses that were actually administered
+        history.each do |date|
+          age_at_date = Synthea::Modules::Lifecycle.age(date, birthdate, nil, :months)
+          # Note: we use drop(1) to non-destructively create a copy of at_months without the first element
+          # so that we don't change the global at_months data
+          at_months = at_months.drop(1) if at_months.size > 0 && age_at_date >= at_months.first && age_at_date - at_months.first < 48
         end
-        return false
+
+        # 3) see if there are any recommended doses remaining that this patient is old enough for
+        return at_months.size > 0 && age_in_months >= at_months.first
+
+        # Note: the approach used here still has some issues, mostly due to odd interactions between
+        # vaccination availability dates and how we schedule adult vaccinations. For example, the PCV13
+        # vaccine is administered to adults over 65, but if the adult is already older than 69 (65 + 4) when
+        # the vaccine is made available in 2012, step 1 above causes the dose to not be administered at all
+
       end
     end
   end
