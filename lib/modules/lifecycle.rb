b73b1e191fbc2390225f1b16c901689fc9663979
Dylan Hall <dehall@mitre.org>
2016-09-12 17:45:02 -0400

    people are not assigned SES factors based on city statistics

--- lib/modules/lifecycle.rb
+++ lib/modules/lifecycle.rb
@@ -21,9 +21,9 @@ module Synthea
           entity[:name_first] = "#{entity[:name_first]}#{(entity[:name_first].hash % 999)}"
           entity[:name_last] = Faker::Name.last_name
           entity[:name_last] = "#{entity[:name_last]}#{(entity[:name_last].hash % 999)}"
-          entity[:gender] = gender unless entity[:gender]
-          entity[:race] = Synthea::World::Demographics::RACES.pick unless entity[:race]
-          entity[:ethnicity] = Synthea::World::Demographics::ETHNICITY[ entity[:race] ].pick unless entity[:ethnicity]
+          entity[:gender] ||= gender
+          entity[:race] ||= Synthea::World::Demographics::RACES.pick
+          entity[:ethnicity] ||= Synthea::World::Demographics::ETHNICITY[ entity[:race] ].pick 
           entity[:blood_type] = Synthea::World::Demographics::BLOOD_TYPES[ entity[:race] ].pick
           # new babies are average weight and length for American newborns
           entity[:height] = 51 # centimeters
@@ -46,10 +46,7 @@ module Synthea
           entity[:address]['line'] << Faker::Address.secondary_address if (rand < 0.5)
           entity[:city] = location_data['city']
           
-          entity[:ses] = { income: rand, education: rand, occupation: rand } 
-          # for now, assign these at birth, purely randomly
-          # eventually these should be given a range based on city. (for example weston MA => small range of income score weighted high, boston = wide range of income score)
-          # also, they should be able to change. for example major illness before age 18 could lead to a reduced education
+          choose_socioeconomic_values(entity)
 
           # TODO update awareness
         end
@@ -215,6 +212,41 @@ module Synthea
         else
           raise "socioeconomic score #{score} outside expected range, make sure weightings add to 1 and categories cover 0..1"
         end
+      end
+
+      def choose_socioeconomic_values(entity)
+        # for now, these are assigned at birth
+        # eventually these should be able to change. for example major illness before age 18 could lead to a reduced education
+        entity[:ses] = { } 
+
+        if entity[:income]
+          # simple linear formula just maps federal poverty level to 0.0 and 75,000 to 1.0
+          # 75,000 chosen based on https://www.princeton.edu/~deaton/downloads/deaton_kahneman_high_income_improves_evaluation_August2010.pdf
+
+          # (11000, 0) -> (75000, 1)
+          # m = y2-y1/x2-x1 = 1/64000
+          # y = mx+b, y = x/64000 - 11/64
+          if entity[:income] >= 75_000
+            entity[:ses][:income] = 1.0
+          elsif entity[:income] <= 11_000
+            entity[:ses][:income] = 0.0
+          else
+            entity[:ses][:income] = entity[:income].to_f / 64_000 - 11.0/64.0
+          end
+        else
+          entity[:ses][:income] = rand
+        end
+
+        edu_scores = Synthea::Config.socioeconomic_status.values.education
+
+        if entity[:education].nil?
+          entity[:ses][:education] = rand
+        else
+          range = edu_scores.send(entity[:education])
+          entity[:ses][:education] = rand(range[0]..range[1])
+        end
+
+        entity[:ses][:occupation] = rand # by default occupation is only 10% of SES and is tough to quantify, so just make it random
       end
 
       # This returns a random integer in a supplied range, possibly weighted; weighting ranges from 0 (prefer
