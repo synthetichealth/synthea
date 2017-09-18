1e35748abeb0fe596bf6b15e6c2b569cfb5db2af
Sheikhnureldin, Abdulrahim S <abduls@gwmail.gwu.edu>
2017-09-18 11:15:13 -0400

    tobacco screening reduces risk of CVD

--- src/main/java/org/mitre/synthea/modules/CommunityHealthWorker.java
+++ src/main/java/org/mitre/synthea/modules/CommunityHealthWorker.java
@@ -208,6 +208,32 @@ public class CommunityHealthWorker extends Provider {
 			int numberOfYearsSmoking = (int) person.ageInYears(time) - 15;
 			probability += (quit_smoking_chw_delta / (smoking_duration_factor_per_year * numberOfYearsSmoking));
 			person.attributes.put(LifecycleModule.QUIT_SMOKING_PROBABILITY, probability);
+			
+			if(person.attributes.containsKey("cardio_risk")){
+				double cardioRisk = (double) person.attributes.get("cardio_risk");
+				cardioRisk = cardioRisk / (4 + quit_smoking_chw_delta);
+			    person.attributes.put("cardio_risk", Utilities.convertRiskToTimestep(cardioRisk, TimeUnit.DAYS.toMillis(3650)));
+			}
+
+			if(person.attributes.containsKey("atrial_fibrillation_risk")){
+				double af_risk = (double) person.attributes.get("atrial_fibrillation_risk");
+				af_risk = af_risk / (4 + quit_smoking_chw_delta);
+				person.attributes.put("atrial_fibrillation_risk", Utilities.convertRiskToTimestep(af_risk, TimeUnit.DAYS.toMillis(3650)));
+				System.out.println("AFFFFFF" + person.attributes.get("atrial_fibrillation_risk"));
+			}
+			
+			if(person.attributes.containsKey("stroke_risk")){
+				double stroke_risk = (double) person.attributes.get("stroke_risk");
+				stroke_risk = stroke_risk / (4 + quit_smoking_chw_delta);
+				person.attributes.put("stroke_risk", Utilities.convertRiskToTimestep(stroke_risk, TimeUnit.DAYS.toMillis(3650)));
+			}
+			
+			if(person.attributes.containsKey("stroke_points")){
+				int stroke_points = (int) person.attributes.get("stroke_points");
+				stroke_points = stroke_points - 2;
+				person.attributes.put("stroke_points", Math.max(0, stroke_points));
+			}	
+			
 		}
 	}
 	
@@ -217,7 +243,7 @@ public class CommunityHealthWorker extends Provider {
 
 		if((boolean) person.attributes.getOrDefault(Person.ALCOHOLIC, false) && this.offers(ALCOHOL_SCREENING)) 
 		{
-			//TODO will have to rework alcoholism to accommodate screenings for all adults, not just alcoholics
+			//TODO rework alcoholism to accommodate screenings for all adults, not just alcoholics
 			Procedure ct = person.record.procedure(time, "Screening for alcohol abuse (procedure)");
 			
 			ct.codes.add(new Code("SNOMED-CT","713107002","Screening for alcohol abuse (procedure)"));
@@ -296,13 +322,10 @@ public class CommunityHealthWorker extends Provider {
 	
 	private void dietPhysicalActivityCounseling(Person person, long time){
 		if (this.offers(DIET_PHYSICAL_ACTIVITY) && person.getVitalSign(VitalSign.BMI) >= 25.0){
-			//add obese/overweight BMI condition above
-
 			//TODO metabolic syndrome module, colorectal cancer module
 
 			// only for adults who have CVD risk factors
 			// the exact threshold for CVD risk factors can be determined later
-			// TODO this should also apply to obese/overweight adults
 			
 			double diet_physical_activity_chw_delta = Double.parseDouble( Config.get("lifecycle.diet_physical_activity.chw_delta", "0.1"));
 
