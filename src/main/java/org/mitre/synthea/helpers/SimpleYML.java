12b7cbb361027882ed150417a3679dc6cb698e9c
Dylan Hall <dehall@mitre.org>
2017-11-08 16:28:30 -0500

    code comments

--- src/main/java/org/mitre/synthea/helpers/SimpleYML.java
+++ src/main/java/org/mitre/synthea/helpers/SimpleYML.java
@@ -4,25 +4,57 @@ import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
 
-
+/**
+ * Helper class to parse a YAML file into a (hopefully) useable map format.
+ * This is useful for arbitrary YAML which doesn't try to adhere to any schema.
+ * (If it does adhere to a schema, there are other methods to parse YAML to objects which should be preferred)
+ * 
+ * Java doesn't have dynamic typing so this class exposes a "get" method which takes a path string,
+ * and walks the map to find the desired item.
+ */
 public class SimpleYML {
 
-  private Map<String,Object> internalMap;
+  /**
+   * The internal representation of the YAML file.
+   */
+  private Map<String,?> internalMap;
   
+  /**
+   * Create a new SimpleYML from the String contents from a file.
+   * 
+   * @param rawContent YML file as a String
+   */
   public SimpleYML(String rawContent)
   {
     Yaml yaml = new Yaml();
-    internalMap = (Map)yaml.load(rawContent);
+    internalMap = (Map<String,?>)yaml.load(rawContent);
   }
   
+  /**
+   * Get the object at the given path from the YML.
+   * For example, given the following YML:<pre>
+   * foo:
+   *    bar: 2
+   *    baz: 4
+   *    qux:
+   *      corge: 9
+   *      grault: -1
+   *  plugh: 7
+   * </pre>
+   * 
+   * calling get("foo.qux.grault") will return Integer(-1)
+   * 
+   * @param path
+   * @return
+   */
   public Object get(String path) {
     String[] pathSegments = path.split("\\.");
     
-    Map<String,Object> current = internalMap;
+    Map<String,?> current = internalMap;
     
     // note: length-2 because we don't want to get the last piece as a map
     for (int i = 0 ; i <= pathSegments.length - 2 ; i++) {
-      current = (Map) current.get( pathSegments[i] );
+      current = (Map<String,?>) current.get( pathSegments[i] );
     }
     
     return current.get(  pathSegments[ pathSegments.length - 1 ]  );
--- src/main/java/org/mitre/synthea/world/concepts/BiometricsConfig.java
+++ src/main/java/org/mitre/synthea/world/concepts/BiometricsConfig.java
@@ -8,18 +8,31 @@ import java.util.stream.Collectors;
 
 import org.mitre.synthea.helpers.SimpleYML;
 
+/**
+ * Class exposing various configurable biometric settings, which are set in resources/biometrics.yml.
+ * 
+ * Unlike the standard "Config" and synthea.properties, it is unlikely that an end-user will want to tweak these often.
+ */
 public abstract class BiometricsConfig {
 
-  private static final SimpleYML biometrics;
+  /**
+   * The SimpleYML containing the parsed 'biometrics.yml' file.
+   */
+  private static final SimpleYML biometrics = loadYML();
   
-  static {
+  /**
+   * Load the 'biometrics.yml' resource file.
+   * 
+   * @return the parsed file
+   */
+  private static final SimpleYML loadYML() {
     try {
       InputStream stream = BiometricsConfig.class.getResourceAsStream("/biometrics.yml");
       //read all text into a string
       String yml = new BufferedReader(new InputStreamReader(stream)).lines()
           .collect(Collectors.joining("\n"));
 
-      biometrics = new SimpleYML(yml);
+      return new SimpleYML(yml);
       
     } catch (Exception e) {
       e.printStackTrace();
@@ -27,15 +40,47 @@ public abstract class BiometricsConfig {
     } 
   }
   
+  /**
+   * Get the object at the given path from the YML.
+   * For example, given the following YML:<pre>
+   * foo:
+   *    bar: 2
+   *    baz: 4
+   *    qux:
+   *      corge: 9
+   *      grault: -1
+   *  plugh: 7
+   * </pre>
+   * 
+   * calling get("foo.qux.grault") will return Integer(-1)
+   * 
+   * @param path path to the desired object
+   * @return the object at path
+   */
   public static Object get(String path) {
     return biometrics.get(path);
   }
   
+  /**
+   * Get the object at the given path from the YML.
+   * If not found, returns the default value.
+   * @param path path to the desired object
+   * @param defaultValue value returned if there is no object at the given path 
+   * @return the object at path, or defaultValue if not found
+   */
   public static Object get(String path, Object defaultValue) {
     Object result = get(path);
     return (result == null) ? defaultValue : result;
   }
   
+  /**
+   * Helper function to get a list of ints from a given location as an array.
+   * Note that this function does not convert doubles to integers,
+   * so if the YML contains doubles then this method may throw.
+   * 
+   * @param path path to the desired object
+   * @return parsed integer array
+   */
   public static int[] ints(String path)
   {
     List<Integer> ints = (List<Integer>)get(path);
@@ -43,6 +88,14 @@ public abstract class BiometricsConfig {
     return array;
   }
   
+  /**
+   * Helper function to get a list of doubles from a given location as an array.
+   * This function does convert integers to doubles, so it should be preferred over ints()
+   * if the data type is unknown or may change.
+   * 
+   * @param path path to the desired object
+   * @return parsed double array
+   */
   public static double[] doubles(String path)
   {
     List<Number> doubles = (List<Number>)get(path);
