a013e5b168538f7b850f9caf7eb39a2b0477e467
Dylan Hall <dehall@mitre.org>
2017-12-11 13:03:26 -0500

    remove costs

--- src/main/java/org/mitre/synthea/world/concepts/Costs.java
+++ src/main/java/org/mitre/synthea/world/concepts/Costs.java
@@ -4,46 +4,22 @@ import org.mitre.synthea.world.concepts.HealthRecord.Entry;
 
 public class Costs {
 
+  /**
+   * Load all cost data needed by the system.
+   */
   public static void loadCostData() {
-    BillingConcept.loadConceptMappings();
-    RelativeValueUnit.loadRVUs();
-    GeographicalPracticeCostIndex.loadGpciData();
+    // TODO: stub. fill this in once we have meaningful cost data to load
   }
 
+  /**
+   * Calculate the cost of this Procedure, Encounter, Medication, etc.
+   * 
+   * @param entry Entry to calculate cost of.
+   * @param isFacility Whether to use facility-based cost factors.
+   * @return Cost, in USD.
+   */
   public static double calculateCost(Entry entry, boolean isFacility) {
-    String syntheaCode = entry.codes.get(0).code;
-
-    // get hcpc mapping from concepts
-    BillingConcept cncpt = BillingConcept.getConcept(syntheaCode);
-    String hcpcCode;
-    if (cncpt == null || cncpt.getHcpcCode() == null) {
-      // default to total hip replacement procedure if no hcpc mapping
-      hcpcCode = "27130";
-    } else {
-      hcpcCode = cncpt.getHcpcCode();
-    }
-
-    // get rvu object using hcpc
-    RelativeValueUnit rvu = RelativeValueUnit.getRvu(hcpcCode);
-    double workRvu = rvu.getWorkRvu();
-    double malpracticeRvu = rvu.getMalpracticeRvu();
-    double pracExpenseRvu = rvu.getNonfacilityPeRvu();
-    if (isFacility) {
-      pracExpenseRvu = rvu.getFacilityPeRvu();
-    }
-
-    // TODO - get locality from config/properties
-    String locality = "REST OF MASSACHUSETTS";
-
-    // use year 2017
-    GeographicalPracticeCostIndex gpci = GeographicalPracticeCostIndex.getGpci(locality);
-    double workGpci = gpci.getPwGpci2017();
-    double pracExpenseGpci = gpci.getPeGpci2017();
-    double malpracticeGpci = gpci.getMpGpci2017();
-
-    // medicare equation
-    double conversionFactor = 35.7751;
-    return (workRvu * workGpci + pracExpenseRvu * pracExpenseGpci
-        + malpracticeRvu * malpracticeGpci) * conversionFactor;
+    // TODO: stub. fill this in once we have meaningful cost data
+    return 0.0;
   }
 }
