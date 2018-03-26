700f2e1009d2ddd900dc87f3c28656ed3b48173a
Carlton Duffett <carlton.duffett@gmail.com>
2018-03-25 18:57:41 -0700

    Add Body Site to ImagingStudy state

--- src/main/java/Graphviz.java
+++ src/main/java/Graphviz.java
@@ -378,10 +378,19 @@ public class Graphviz {
         break;
       case "ImagingStudy":
         JsonArray series = state.get("series").getAsJsonArray();
+
         JsonObject modality = series.get(0).getAsJsonObject().get("modality").getAsJsonObject();
-        String code = modality.get("code").getAsString();
-        String display = modality.get("display").getAsString();
-        details.append("DICOM-DCM[").append(code).append("]: ").append(display);
+        String modalityCode = modality.get("code").getAsString();
+        String modalityDisplay = modality.get("display").getAsString();
+        details.append("DICOM-DCM[").append(modalityCode).append("]: ").append(modalityDisplay)
+            .append(NEWLINE);
+
+        JsonObject bodySite = series.get(0).getAsJsonObject().get("body_site").getAsJsonObject();
+        String bodySiteCode = bodySite.get("code").getAsString();
+        String bodySiteDisplay = bodySite.get("display").getAsString();
+
+        details.append("SNOMED-CT[").append(bodySiteCode).append("] Body Site: ")
+            .append(bodySiteDisplay).append(NEWLINE);
       default:
         // no special description
     }
