86975996f5e7d7c03a9eb34876fa94e1a9b75888
Carlton Duffett <carlton.duffett@gmail.com>
2018-03-28 22:19:14 -0700

    Add ImagingStudy to CCDAExporter

--- src/main/java/org/mitre/synthea/export/CCDAExporter.java
+++ src/main/java/org/mitre/synthea/export/CCDAExporter.java
@@ -1,12 +1,16 @@
 package org.mitre.synthea.export;
 
 import freemarker.template.Configuration;
+import freemarker.template.SimpleNumber;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import freemarker.template.TemplateMethodModelEx;
 
 import java.io.StringWriter;
+import java.util.List;
 import java.util.UUID;
 
+import org.mitre.synthea.helpers.Utilities;
 import org.mitre.synthea.world.agents.Person;
 import org.mitre.synthea.world.concepts.HealthRecord.Encounter;
 import org.mitre.synthea.world.concepts.RaceAndEthnicity;
@@ -24,6 +28,18 @@ public class CCDAExporter {
   private static final Object UUID_GEN = new Object() {
     public String toString() {
       return UUID.randomUUID().toString();
+    }
+  };
+
+  /**
+   * Generates a random DICOM UID for a study, instance, or series
+   * in a CCDA template.
+   */
+  private static class RandomDicomUidMethod implements TemplateMethodModelEx {
+    public Object exec(List args) {
+      Number seriesNo = ((SimpleNumber) args.get(0)).getAsNumber();
+      Number instanceNo = ((SimpleNumber) args.get(1)).getAsNumber();
+      return Utilities.randomDicomUid(seriesNo.intValue(), instanceNo.intValue());
     }
   };
 
@@ -46,7 +62,7 @@ public class CCDAExporter {
 
   /**
    * Export a CCDA R2.1 document for a Person at a given time.
-   * 
+   *
    * @param person
    *          Person to export.
    * @param time
@@ -69,13 +85,16 @@ public class CCDAExporter {
         superEncounter.immunizations.addAll(encounter.immunizations);
         superEncounter.medications.addAll(encounter.medications);
         superEncounter.careplans.addAll(encounter.careplans);
+        superEncounter.imagingStudies.addAll(encounter.imagingStudies);
       } else {
         break;
       }
     }
+
     // The export templates fill in the record by accessing the attributes
     // of the Person, so we add a few attributes just for the purposes of export.
     person.attributes.put("UUID", UUID_GEN);
+    person.attributes.put("randomDicomUid", new RandomDicomUidMethod());
     person.attributes.put("ehr_encounters", person.record.encounters);
     person.attributes.put("ehr_observations", superEncounter.observations);
     person.attributes.put("ehr_reports", superEncounter.reports);
@@ -85,6 +104,7 @@ public class CCDAExporter {
     person.attributes.put("ehr_immunizations", superEncounter.immunizations);
     person.attributes.put("ehr_medications", superEncounter.medications);
     person.attributes.put("ehr_careplans", superEncounter.careplans);
+    person.attributes.put("ehr_imaging_studies", superEncounter.imagingStudies);
     person.attributes.put("time", time);
     person.attributes.put("race_lookup", RaceAndEthnicity.LOOK_UP_CDC_RACE);
     person.attributes.put("ethnicity_lookup", RaceAndEthnicity.LOOK_UP_CDC_ETHNICITY_CODE);
