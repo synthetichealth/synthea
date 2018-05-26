d0d0c34162675746a2ffb3c474bf21e209dabf48
Jason Walonoski <jwalonoski@mitre.org>
2018-05-26 12:45:45 -0400

    Add CDW Immunizations.

--- src/main/java/org/mitre/synthea/world/concepts/HealthRecord.java
+++ src/main/java/org/mitre/synthea/world/concepts/HealthRecord.java
@@ -150,6 +150,8 @@ public class HealthRecord {
   }
 
   public class Immunization extends Entry {
+    public int series = -1;
+
     public Immunization(long start, String type) {
       super(start, type);
     }
@@ -291,7 +293,7 @@ public class HealthRecord {
     public List<Entry> conditions;
     public List<Entry> allergies;
     public List<Procedure> procedures;
-    public List<Entry> immunizations;
+    public List<Immunization> immunizations;
     public List<Medication> medications;
     public List<CarePlan> careplans;
     public List<ImagingStudy> imagingStudies;
@@ -319,7 +321,7 @@ public class HealthRecord {
       conditions = new ArrayList<Entry>();
       allergies = new ArrayList<Entry>();
       procedures = new ArrayList<Procedure>();
-      immunizations = new ArrayList<Entry>();
+      immunizations = new ArrayList<Immunization>();
       medications = new ArrayList<Medication>();
       careplans = new ArrayList<CarePlan>();
       imagingStudies = new ArrayList<ImagingStudy>();
