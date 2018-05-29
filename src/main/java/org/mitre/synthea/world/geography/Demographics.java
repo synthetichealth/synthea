5015bc3e15943881f1abd88d60dc63570cd2416e
Dylan Hall <dehall@mitre.org>
2018-05-29 10:11:01 -0400

    initial fix for demographics, specificially age, not matching target %s

--- src/main/java/org/mitre/synthea/world/geography/Demographics.java
+++ src/main/java/org/mitre/synthea/world/geography/Demographics.java
@@ -91,7 +91,7 @@ public class Demographics {
     return raceDistribution.next(random);
   }
 
-  public String ethnicityFromRace(String race, Person person) {
+  public String ethnicityFromRace(String race, Random random) {
     // https://en.wikipedia.org/wiki/Demographics_of_Massachusetts#Race.2C_ethnicity.2C_and_ancestry
     if (race.equals("white")) {
       RandomCollection<String> whiteEthnicities = new RandomCollection<>();
@@ -108,25 +108,25 @@ public class Demographics {
       whiteEthnicities.add(1.9, "russian");
       whiteEthnicities.add(1.8, "swedish");
       whiteEthnicities.add(1.2, "greek");
-      return whiteEthnicities.next(person.random);
+      return whiteEthnicities.next(random);
     } else if (race.equals("hispanic")) {
       RandomCollection<String> hispanicEthnicities = new RandomCollection<>();
       hispanicEthnicities.add(4.1, "puerto_rican");
       hispanicEthnicities.add(1, "mexican");
       hispanicEthnicities.add(1, "central_american");
       hispanicEthnicities.add(1, "south_american");
-      return hispanicEthnicities.next(person.random);
+      return hispanicEthnicities.next(random);
     } else if (race.equals("black")) {
       RandomCollection<String> blackEthnicities = new RandomCollection<>();
       blackEthnicities.add(1.8, "african");
       blackEthnicities.add(1.8, "dominican");
       blackEthnicities.add(1.8, "west_indian");
-      return blackEthnicities.next(person.random);
+      return blackEthnicities.next(random);
     } else if (race.equals("asian")) {
       RandomCollection<String> asianEthnicities = new RandomCollection<>();
       asianEthnicities.add(2.0, "chinese");
       asianEthnicities.add(1.1, "asian_indian");
-      return asianEthnicities.next(person.random);
+      return asianEthnicities.next(random);
     } else if (race.equals("native")) {
       return "american_indian";
     } else { // race.equals("other")
@@ -134,7 +134,7 @@ public class Demographics {
     }
   }
 
-  public String languageFromEthnicity(String ethnicity, Person person) {
+  public String languageFromEthnicity(String ethnicity, Random random) {
     // https://apps.mla.org/map_data -> search by State MA
     // or see
     // https://apps.mla.org/map_data_results&SRVY_YEAR=2010&geo=state&state_id=25&county_id=
@@ -158,95 +158,95 @@ public class Demographics {
       RandomCollection<String> italianLanguages = new RandomCollection<>();
       italianLanguages.add(95.0, "english");
       italianLanguages.add(5.0, "italian");
-      return italianLanguages.next(person.random);
+      return italianLanguages.next(random);
     } else if (ethnicity.equals("french")) {
       RandomCollection<String> frenchLanguages = new RandomCollection<>();
       frenchLanguages.add(99.0, "english");
       frenchLanguages.add(1.0, "french");
-      return frenchLanguages.next(person.random);
+      return frenchLanguages.next(random);
     } else if (ethnicity.equals("french_canadian")) {
       RandomCollection<String> frenchCanadianLanguages = new RandomCollection<>();
       frenchCanadianLanguages.add(99.0, "english");
       frenchCanadianLanguages.add(1.0, "french");
-      return frenchCanadianLanguages.next(person.random);
+      return frenchCanadianLanguages.next(random);
     } else if (ethnicity.equals("german")) {
       RandomCollection<String> germanLanguages = new RandomCollection<>();
       germanLanguages.add(96.0, "english");
       germanLanguages.add(4.0, "german");
-      return germanLanguages.next(person.random);
+      return germanLanguages.next(random);
     } else if (ethnicity.equals("polish")) {
       return "english";
     } else if (ethnicity.equals("portuguese")) {
       RandomCollection<String> portugueseLanguages = new RandomCollection<>();
       portugueseLanguages.add(37.0, "english");
       portugueseLanguages.add(63.0, "portuguese");
-      return portugueseLanguages.next(person.random);
+      return portugueseLanguages.next(random);
     } else if (ethnicity.equals("russian")) {
       RandomCollection<String> russianLanguages = new RandomCollection<>();
       russianLanguages.add(62.0, "english");
       russianLanguages.add(38.0, "russian");
-      return russianLanguages.next(person.random);
+      return russianLanguages.next(random);
     } else if (ethnicity.equals("swedish")) {
       return "english";
     } else if (ethnicity.equals("greek")) {
       RandomCollection<String> greekLanguages = new RandomCollection<>();
       greekLanguages.add(66.0, "english");
       greekLanguages.add(34.0, "greek");
-      return greekLanguages.next(person.random);
+      return greekLanguages.next(random);
     } else if (ethnicity.equals("puerto_rican")) {
       RandomCollection<String> puertoRicanLanguages = new RandomCollection<>();
       puertoRicanLanguages.add(30.0, "english");
       puertoRicanLanguages.add(70.0, "spanish");
-      return puertoRicanLanguages.next(person.random);
+      return puertoRicanLanguages.next(random);
     } else if (ethnicity.equals("mexican")) {
       RandomCollection<String> mexicanLanguages = new RandomCollection<>();
       mexicanLanguages.add(30.0, "english");
       mexicanLanguages.add(70.0, "spanish");
-      return mexicanLanguages.next(person.random);
+      return mexicanLanguages.next(random);
     } else if (ethnicity.equals("central_american")) {
       RandomCollection<String> centralAmericanLanguages = new RandomCollection<>();
       centralAmericanLanguages.add(30.0, "english");
       centralAmericanLanguages.add(70.0, "spanish");
-      return centralAmericanLanguages.next(person.random);
+      return centralAmericanLanguages.next(random);
     } else if (ethnicity.equals("south_american")) {
       RandomCollection<String> southAmericanLanguages = new RandomCollection<>();
       southAmericanLanguages.add(30.0, "english");
       southAmericanLanguages.add(35.0, "spanish");
       southAmericanLanguages.add(35.0, "portuguese");
-      return southAmericanLanguages.next(person.random);
+      return southAmericanLanguages.next(random);
     } else if (ethnicity.equals("african")) {
       RandomCollection<String> africanLanguages = new RandomCollection<>();
       africanLanguages.add(95.0, "english");
       africanLanguages.add(5.0, "french");
-      return africanLanguages.next(person.random);
+      return africanLanguages.next(random);
     } else if (ethnicity.equals("dominican")) {
       RandomCollection<String> dominicanLanguages = new RandomCollection<>();
       dominicanLanguages.add(30.0, "english");
       dominicanLanguages.add(70.0, "spanish");
-      return dominicanLanguages.next(person.random);
+      return dominicanLanguages.next(random);
     } else if (ethnicity.equals("west_indian")) {
       RandomCollection<String> westIndianLanguages = new RandomCollection<>();
       westIndianLanguages.add(25.0, "english");
       westIndianLanguages.add(35.0, "spanish");
       westIndianLanguages.add(50.0, "french_creole");
-      return westIndianLanguages.next(person.random);
+      return westIndianLanguages.next(random);
     } else if (ethnicity.equals("chinese")) {
       RandomCollection<String> chineseLanguages = new RandomCollection<>();
       chineseLanguages.add(25.0, "english");
       chineseLanguages.add(75.0, "chinese");
-      return chineseLanguages.next(person.random);
+      return chineseLanguages.next(random);
     } else if (ethnicity.equals("asian_indian")) {
       RandomCollection<String> asianIndianLanguages = new RandomCollection<>();
       asianIndianLanguages.add(75.0, "english");
       asianIndianLanguages.add(25.0, "hindi");
-      return asianIndianLanguages.next(person.random);
+      return asianIndianLanguages.next(random);
     } else if (ethnicity.equals("american_indian")) {
       return "english";
     } else if (ethnicity.equals("arab")) {
       RandomCollection<String> arabLanguages = new RandomCollection<>();
       arabLanguages.add(63.0, "english");
       arabLanguages.add(37.0, "arabic");
-      return arabLanguages.next(person.random);
+      return arabLanguages.next(random);
     } else { // in case of invalid ethnicity
       return "english";
     }
@@ -308,7 +308,7 @@ public class Demographics {
     return educationDistribution.next(random);
   }
 
-  public double educationLevel(String level, Person person) {
+  public double educationLevel(String level, Random random) {
     double lessThanHsMin = Double.parseDouble(
         Config.get("generate.demographics.socioeconomic.education.less_than_hs.min", "0.0"));
     double lessThanHsMax = Double.parseDouble(
@@ -328,16 +328,20 @@ public class Demographics {
 
     switch (level) {
       case "less_than_hs":
-        return person.rand(lessThanHsMin, lessThanHsMax);
+        return rand(random, lessThanHsMin, lessThanHsMax);
       case "hs_degree":
-        return person.rand(hsDegreeMin, hsDegreeMax);
+        return rand(random, hsDegreeMin, hsDegreeMax);
       case "some_college":
-        return person.rand(someCollegeMin, someCollegeMax);
+        return rand(random, someCollegeMin, someCollegeMax);
       case "bs_degree":
-        return person.rand(bsDegreeMin, bsDegreeMax);
+        return rand(random, bsDegreeMin, bsDegreeMax);
       default:
         return 0.0;
     }
+  }
+  
+  private static double rand(Random r, double low, double high) {
+    return (low + ((high - low) * r.nextDouble()));
   }
 
   public double socioeconomicScore(double income, double education, double occupation) {
