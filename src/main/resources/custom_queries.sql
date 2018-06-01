57f88371c643ed83e943756d1bfe92b8b121820e
Dylan Hall <dehall@mitre.org>
2018-06-01 17:34:01 -0400

    comment out sample queries

--- src/main/resources/custom_queries.sql
+++ src/main/resources/custom_queries.sql
@@ -13,4 +13,4 @@
 
 -- Sample Query 3: select the people that have an active diagnosis of diabetes, along with the age of diagnosis
 
- select p.name, p.gender, DATEADD('MILLISECOND', p.DATE_OF_BIRTH, DATE '1970-01-01') DOB, DATEADD('MILLISECOND', c.start , DATE '1970-01-01') onset_date, DATEDIFF('YEAR', DATEADD('MILLISECOND', p.DATE_OF_BIRTH, DATE '1970-01-01'), DATEADD('MILLISECOND', c.start, DATE '1970-01-01')) age_at_diagnosis   FROM PERSON p, CONDITION c WHERE p.ID = c.PERSON_ID AND c.code = '44054006';
+-- select p.name, p.gender, DATEADD('MILLISECOND', p.DATE_OF_BIRTH, DATE '1970-01-01') DOB, DATEADD('MILLISECOND', c.start , DATE '1970-01-01') onset_date, DATEDIFF('YEAR', DATEADD('MILLISECOND', p.DATE_OF_BIRTH, DATE '1970-01-01'), DATEADD('MILLISECOND', c.start, DATE '1970-01-01')) age_at_diagnosis   FROM PERSON p, CONDITION c WHERE p.ID = c.PERSON_ID AND c.code = '44054006';
