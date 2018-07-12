a12baa9ff04846eff7aefa5a7883f0d2c6812e80
Keeyan <keeyan.ghoreshi@uconn.edu>
2018-07-12 17:07:55 -0400

    Fixed issues and recommitted after soft reset and checkout of new branch

---
+++ src/main/java/org/mitre/synthea/world/concepts/Terminology.java
@@ -0,0 +1,516 @@
+package org.mitre.synthea.world.concepts;
+
+import ca.uhn.fhir.context.FhirContext;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+
+
+import javafx.util.Pair;
+
+import okhttp3.*;
+import org.hl7.fhir.dstu3.model.ValueSet;
+import org.mitre.synthea.helpers.Config;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+
+/**
+ * Finds a specified value set, extracts the codes and systems and selects one code/system at random.
+ */
+
+public class Terminology {
+
+    private static String VS_FOLDER = "valuesets";
+    private static String URL_SCHEME = "https";
+
+    private static String PROXY_URL = Config.get("terminology.PROXY_HOSTNAME");
+
+    private static int PROXY_PORT = Integer.parseInt(Config.get("terminology.PORT"));
+
+    private static final String VSAC_USER = Config.get("terminology.username");
+    private static final String VSAC_PASS = Config.get("terminology.password");
+
+    // URL needs to be HTTPS or the request will be redirected, defaulting request type
+    private static final String AUTH_URL = "https://vsac.nlm.nih.gov/vsac/ws/Ticket";
+    private static final String SERVICE_URL = "http://umlsks.nlm.nih.gov";
+    private static final String VSAC_VALUE_SET_RETRIEVAL_URL = "vsac.nlm.nih.gov";
+
+    private static final FhirContext ctx = FhirContext.forDstu3();
+    private static final Map<String, ValueSet> valueSets = loadValueSets();
+
+    private static String defaultCode;
+    private static String defaultSystem;
+
+    final static Logger logger = LoggerFactory.getLogger(Terminology.class);
+
+
+
+    public static class Session{
+        private OkHttpClient client;
+
+
+        // Can be reused for multiple requests
+        private String authToken;
+
+        public Session(){
+            if(PROXY_URL!=null){
+                client = getClient(PROXY_URL,PROXY_PORT);
+            }else{
+                client = getClient();
+
+            }
+            if(VSAC_PASS == null | VSAC_USER==null){
+                authToken=null;
+            }else{
+                authToken = Terminology.getAuthToken(client);
+            }
+
+
+
+
+        }
+
+
+        /**
+         *
+         * @param url The canonical URL of the ValueSet or a VSAC oid
+         * @return A random code from the specified ValueSet as a Pair of form <system:code>
+         */
+        public Pair<String,String> getRandomCode(String url){
+
+            ValueSet vs = Terminology.getValueSet(url);
+            if(vs==null & authToken!=null){
+
+                String ticket = getServiceTicket(authToken,client);
+
+                Response response = Terminology.getValueSet(client,url,ticket);
+                if(response.code()==200){
+                    try{
+                        vs = parseResponse(response.body().string());
+                        valueSets.put(url,vs);
+
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }else{
+                    logger.warn("Invalid OID or URL provided");
+                    return new Pair<>(defaultSystem,defaultCode);
+                }
+            }else if(vs==null){
+                return new Pair<>(defaultSystem,defaultCode);
+            }
+            return chooseCode(Terminology.getCodes(vs));
+
+        }
+
+        /**
+         *
+         * @param xml String representation of XML response from VSAC
+         * @return ValueSet containing systems and their codes
+         * @throws Exception
+         */
+        public ValueSet parseResponse(String xml) throws Exception
+        {
+            // Parse the string to get XML encoding
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(xml));
+
+            // Build a ValueSet
+            ValueSet vs = new ValueSet();
+
+            /*A value set should look like
+             Compose: {
+                  Include: [
+                      {
+                      system,
+                      concept: [
+                              code
+                              code
+                              ...
+                              code
+                               ]
+                      }
+                      {
+                      system,
+                      concept: [
+                          ...
+                              ]
+                       }
+                       ]}
+            */
+            ValueSet.ValueSetComposeComponent composeComponent = new ValueSet.ValueSetComposeComponent();
+            Document responseXml = builder.parse(is);
+            Element root = responseXml.getDocumentElement();
+            String namespaceURI = root.getNamespaceURI();
+            NodeList concepts =responseXml.getElementsByTagNameNS(namespaceURI,"Concept");
+            for(int i = 0 ; i<concepts.getLength();i++){
+
+                Node currentNode = concepts.item(i);
+                String system = currentNode.getAttributes().getNamedItem("codeSystem").getTextContent();
+                String code = currentNode.getAttributes().getNamedItem("code").getTextContent();
+                String display = currentNode.getAttributes().getNamedItem("displayName").getTextContent();
+
+                // the toggle checks to see if we already have the system present
+                // in the ValueSet so we don't have duplicates with the codes
+                // spread out between them.
+                boolean toggle = false;
+                for (ValueSet.ConceptSetComponent c : composeComponent.getInclude()) {
+                    if(c.getSystem().equals(system)){
+                        c.addConcept().setCode(code).setDisplay(display);
+                        toggle=true;
+
+                    }
+
+                }
+                if(!toggle){
+                    composeComponent.addInclude().setSystem(system).addConcept().setCode(code);
+                }
+
+            }
+            vs.setCompose(composeComponent);
+//            System.out.println(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(vs));
+
+            return vs;
+
+
+        }
+
+        public <E> Optional<E> getRandom (Collection<E> e) {
+            //Gets random element from java collection
+
+            return e.stream()
+                    .skip((int) (e.size() * Math.random()))
+                    .findFirst();
+        }
+
+        public Pair<String,String> chooseCode(Multimap<String,String> codes){
+            // Chooses a random code by picking a system then picking a code from
+            // that system
+
+            List<String> keys = new ArrayList<>(codes.keySet());
+            Random r = new Random();
+            String system = keys.get(r.nextInt(keys.size()));
+
+            String code = getRandom(codes.get(system)).get();
+
+            return new Pair<>(system,code);
+        }
+
+        public String getAuthToken(){
+            return this.authToken;
+        }
+        public OkHttpClient getSessionClient(){
+            return this.client;
+        }
+        public void setAuthToken(String token){
+            this.authToken=token;
+        }
+
+
+
+
+
+    }
+
+    public static Map<String, ValueSet> loadValueSets(){
+        // Loads valueSets from file and puts them in a map of <url : ValueSet>
+
+        Map<String, ValueSet> retVal = new ConcurrentHashMap<String, ValueSet>();
+        URL valuesetsFolder = ClassLoader.getSystemClassLoader().getResource(VS_FOLDER);
+        try{
+            Path path = Paths.get(valuesetsFolder.toURI());
+
+            Files.walk(path, Integer.MAX_VALUE).filter(Files::isReadable).filter(Files::isRegularFile)
+                    .filter(p -> p.toString().endsWith(".json")).forEach(t -> {
+                try {
+                    ValueSet vs = loadFile(t);
+                    String url = vs.getUrl();
+
+                    retVal.put(url,vs);
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            });
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        return retVal;
+
+    }
+
+    public static ValueSet loadFile(Path path) throws Exception{
+
+        // Loads JSON files as ValueSets
+
+        FileReader fileReader = new FileReader(path.toString());
+        JsonReader reader = new JsonReader(fileReader);
+        JsonParser parser = new JsonParser();
+        String object = parser.parse(reader).toString();
+        ValueSet valueset;
+
+        try{
+            valueset = ctx.newJsonParser().parseResource(ValueSet.class, object);
+        }catch(Exception e){
+            System.out.println("File " + path.toString() + " is not the correct format for a ValueSet");
+            valueset = null;
+        }
+        fileReader.close();
+        reader.close();
+        return valueset;
+    }
+
+    public static ValueSet getValueSet(String url){
+        // Temporary call to get valueSet from a Map
+        return valueSets.get(url);
+    }
+
+
+    public static Multimap<String, String> getCodes(ValueSet vs){
+        /* Gets codes from the provided value set and sorts them by
+          system.
+
+          Sorting by system makes each system have the same "weight" when being chosen
+          e.g. SNOMED may have 100 codes to ICD-10's 3 in a value set but the chance that
+          either one gets picked is the same.
+         */
+
+        Multimap<String,String> retVal = ArrayListMultimap.create();
+        String system;
+        String code;
+        List<ValueSet.ConceptSetComponent> concepts = vs.getCompose().getInclude();
+        for(ValueSet.ConceptSetComponent concept : concepts){
+            system = concept.getSystem();
+            for(ValueSet.ConceptReferenceComponent conceptRef : concept.getConcept()){
+                retVal.put(system,conceptRef.getCode());
+            }
+
+        }
+
+        return retVal;
+
+    }
+
+
+
+
+    public static OkHttpClient getClient(String host, int port){
+        // If you want to route through a proxy
+
+        return new OkHttpClient.Builder()
+                .proxy(new Proxy(Proxy.Type.HTTP,
+                        new InetSocketAddress(host, port)))
+                .build();
+    }
+
+    //Overloaded for use without a proxy
+    public static OkHttpClient getClient() {
+        return new OkHttpClient.Builder().build();
+    }
+
+    public static String getAuthToken(OkHttpClient client){
+        String token;
+
+        //Order matters for request body, requiring an ordered map
+        Map<String,String> requestForm = new LinkedHashMap<>();
+        requestForm.put("username",VSAC_USER);
+        requestForm.put("password",VSAC_PASS);
+
+        // Make request body
+        RequestBody requestBody = makeRequestBody(requestForm);
+
+        //Create Request
+        Request request = makeRequest(AUTH_URL,requestBody);
+
+        try {
+            Response response = client.newCall(request).execute();
+            token = response.body().string();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+            token = null;
+            // Check to make sure that params are correctly entered
+            // eg. 'username' not 'user', 'password' not 'pass'.
+        }
+        return token;
+
+
+
+    }
+
+    /**
+     * Service tickets are a one-time use auth code for accessing value sets from VSAC
+     * A regular auth token can be used to acquire multiple service tickets
+     */
+    public static String getServiceTicket(String auth_token, OkHttpClient client){
+        String token;
+
+        Map<String,String> requestForm = new LinkedHashMap<>();
+        requestForm.put("service",SERVICE_URL);
+        RequestBody requestBody = makeRequestBody(requestForm);
+        Request request = makeRequest(AUTH_URL + "/" + auth_token,requestBody);
+
+        try {
+            Response response = client.newCall(request).execute();
+            token = response.body().string();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+            token = null;
+            // Check to make sure that params are correctly entered
+            // eg. 'username' not 'user', 'password' not 'pass'.
+        }
+
+        return token;
+
+
+    }
+
+    public static String makeGenericPost(String url, Map<String,String> requestForm, OkHttpClient client){
+        String response_body;
+        RequestBody requestBody = makeRequestBody(requestForm);
+
+        Request request = makeRequest(url,requestBody);
+        try {
+            Response response = client.newCall(request).execute();
+            response_body = response.body().string();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+            response_body = null;
+        }
+
+        return response_body;
+
+    }
+
+    public static String makeGenericGet(String url, Map<String,String> requestForm, OkHttpClient client){
+        String response_body;
+        HttpUrl requestUrl = makeGetUrl(url, requestForm);
+
+        Request request = makeRequest(requestUrl.toString());
+        try {
+            Response response = client.newCall(request).execute();
+            response_body = response.body().string();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+            response_body = null;
+        }
+
+        return response_body;
+
+    }
+
+    public static Response getValueSet(OkHttpClient client, String oid, String ticket){
+        Response response=null;
+        Map<String,String> parameterMap = new LinkedHashMap<>();
+        parameterMap.put("id",oid);
+        parameterMap.put("ticket",ticket);
+        HttpUrl requestUrl = makeGetUrl(VSAC_VALUE_SET_RETRIEVAL_URL,parameterMap);
+        Request request = makeRequest(requestUrl.toString());
+        try {
+            response = client.newCall(request).execute();
+
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+
+
+
+    }
+
+
+    public static Request makeRequest(String url){
+         //Makes requests of specified type, overloaded to switch between GET and POST
+
+        Request request = new Request.Builder()
+                        .url(url)
+                        .get()
+                        .build();
+
+        return request;
+
+    }
+    public static Request makeRequest(String url, RequestBody body){
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        return request;
+    }
+
+    public static RequestBody makeRequestBody(Map<String,String> valuePairs){
+        // Builds generic request bodies from maps
+
+        FormBody.Builder requestBuilder = new FormBody.Builder();
+        for (String s : valuePairs.keySet()) {
+
+            requestBuilder.add(s,valuePairs.get(s));
+        }
+
+        return requestBuilder.build();
+    }
+
+    public static HttpUrl makeGetUrl(String url, Map<String,String> valuePairs){
+        // Constructs the url that is queried in the Get call
+
+        HttpUrl.Builder urlBuilder = new HttpUrl.Builder();
+        urlBuilder.scheme(URL_SCHEME).host(url).addPathSegments("vsac/svs/RetrieveValueSet");
+        for (String s : valuePairs.keySet()) {
+
+            urlBuilder.setQueryParameter(s,valuePairs.get(s));
+        }
+
+        return urlBuilder.build();
+    }
+
+    public static void setDefaultCode(String code){
+        defaultCode = code;
+    }
+
+    public static String getDefaultCode(){
+        return defaultCode;
+    }
+
+    public static void setDefaultSystem(String system){
+        defaultSystem = system;
+    }
+
+    public static String getDefaultSystem(){
+        return defaultSystem;
+    }
+
+
+
+}
---
+++ src/test/java/org/mitre/synthea/world/concepts/TerminologyTest.java
@@ -0,0 +1,295 @@
+package org.mitre.synthea.world.concepts;
+
+import com.google.common.collect.Multimap;
+import javafx.util.Pair;
+import okhttp3.*;
+import org.hl7.fhir.dstu3.model.ValueSet;
+import org.junit.Before;
+import org.junit.Test;
+import org.mitre.synthea.helpers.Config;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class TerminologyTest {
+    private Terminology.Session session;
+
+    @Before
+    public void setUp() {
+        session = new Terminology.Session();
+        Terminology.setDefaultCode("8675309");
+        Terminology.setDefaultSystem("http://henlo.org/");
+
+    }
+
+    @Test
+    public void testSession() {
+        List<String> resultsList = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Pair<String, String> retVal = session.getRandomCode("https://www.hl7.org/fhir/synthea/diabetes");
+            String code = retVal.getValue();
+            resultsList.add(code);
+            assertTrue(code.equals("427089005") || code.equals("E08") || code.equals("250.00"));
+        }
+        boolean allEqual = resultsList.isEmpty() || resultsList.stream().allMatch(resultsList.get(0)::equals);
+        assertFalse(allEqual);
+
+        resultsList.clear();
+
+        if(session.getAuthToken()!=null){
+            for (int i = 0; i < 8; i++) {
+                Pair<String, String> retVal = session.getRandomCode("2.16.840.1.113883.3.464.1003.198.12.1071");
+                String code = retVal.getValue();
+                resultsList.add(code);
+                assertTrue(code.equals("248802009") || code.equals("V45.71") || code.equals("Z90.10"));
+            }
+            allEqual = resultsList.isEmpty() || resultsList.stream().allMatch(resultsList.get(0)::equals);
+            assertFalse(allEqual);
+            ValueSet va = Terminology.getValueSet("2.16.840.1.113883.3.464.1003.198.12.1071");
+            assertNotNull(va);
+            String placeholder = session.getAuthToken();
+            session.setAuthToken(null);
+            Pair<String, String> cachedResult = session.getRandomCode("2.16.840.1.113883.3.464.1003.198.12.1071");
+            assertNotEquals(cachedResult.getKey(), Terminology.getDefaultSystem());
+            assertNotEquals(cachedResult.getValue(), Terminology.getDefaultCode());
+
+            resultsList.clear();
+            session.setAuthToken(placeholder);
+
+            for (int i = 0; i < 8; i++) {
+                Pair<String, String> retVal = session.getRandomCode("2.16.840.1.113883.3.464.1003.103.12.1001");
+                String code = retVal.getValue();
+                resultsList.add(code);
+
+            }
+
+            boolean enoughUniqueCodes = resultsList.stream().distinct().count() > 3;
+            assertTrue(enoughUniqueCodes);
+        }else{
+            Pair<String,String> retVal = session.getRandomCode("2.16.840.1.113883.3.464.1003.198.12.1071");
+            assertEquals(retVal.getKey(), Terminology.getDefaultSystem());
+            assertEquals(retVal.getValue(), Terminology.getDefaultCode());
+        }
+        Pair<String,String> retVal = session.getRandomCode("1.11.111.1.111111.11.1.111.111.11.111");
+        assertEquals(retVal.getKey(), Terminology.getDefaultSystem());
+        assertEquals(retVal.getValue(), Terminology.getDefaultCode());
+
+
+
+
+
+
+
+
+    }
+
+    @Test
+    public void loadValueSets() {
+        Map<String, ValueSet> valueSetMap = Terminology.loadValueSets();
+        ValueSet va = valueSetMap.get("https://www.hl7.org/fhir/synthea/diabetes");
+        assertTrue(va.hasCompose());
+        assertTrue(va.getCompose().hasInclude());
+
+    }
+
+    @Test
+    public void loadFile() {
+        Path path = Paths.get("src/main/resources/valuesets/diabetes.json");
+        ValueSet va;
+        try {
+            va = Terminology.loadFile(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            va = null;
+        }
+        assertTrue(Objects.requireNonNull(va).hasCompose());
+        assertTrue(va.getCompose().hasInclude());
+        path = Paths.get("src/main/resources/valuesets/neverMakeThisFile.json");
+        try {
+            va = Terminology.loadFile(path);
+        } catch (Exception e) {
+            va = null;
+        }
+        assertNull(va);
+    }
+
+    @Test
+    public void getValueSet() {
+        ValueSet va = Terminology.getValueSet("https://www.hl7.org/fhir/synthea/diabetes");
+        assertNotNull(va);
+        assertTrue(va.hasCompose());
+
+    }
+
+    @Test
+    public void getCodes() {
+        ValueSet va = new ValueSet();
+        va.getCompose().addInclude();
+        ValueSet.ConceptSetComponent b1 = new ValueSet.ConceptSetComponent();
+        ValueSet.ConceptSetComponent b2 = new ValueSet.ConceptSetComponent();
+        b1.setSystem("jimbo");
+        b2.setSystem("skadoosh");
+
+        ValueSet.ConceptReferenceComponent c1 = new ValueSet.ConceptReferenceComponent();
+        ValueSet.ConceptReferenceComponent c2 = new ValueSet.ConceptReferenceComponent();
+        ValueSet.ConceptReferenceComponent c3 = new ValueSet.ConceptReferenceComponent();
+        ValueSet.ConceptReferenceComponent c4 = new ValueSet.ConceptReferenceComponent();
+        ValueSet.ConceptReferenceComponent c5 = new ValueSet.ConceptReferenceComponent();
+        ValueSet.ConceptReferenceComponent c6 = new ValueSet.ConceptReferenceComponent();
+        ValueSet.ConceptReferenceComponent c7 = new ValueSet.ConceptReferenceComponent();
+
+        c1.setCode("1");
+        c2.setCode("2");
+        c3.setCode("3");
+        c4.setCode("4");
+        c5.setCode("5");
+        c6.setCode("6");
+        c7.setCode("7");
+        b1.addConcept(c1);
+        b1.addConcept(c2);
+        b1.addConcept(c3);
+        b2.addConcept(c4);
+        b2.addConcept(c5);
+        b2.addConcept(c6);
+        b2.addConcept(c7);
+        va.getCompose().addInclude(b1);
+        va.getCompose().addInclude(b2);
+        Multimap<String, String> codeMap = Terminology.getCodes(va);
+        assertTrue(codeMap.containsKey("skadoosh"));
+        assertTrue(codeMap.get("skadoosh").contains("4"));
+        assertTrue(codeMap.get("skadoosh").contains("5"));
+        assertTrue(codeMap.get("skadoosh").contains("6"));
+        assertTrue(codeMap.get("skadoosh").contains("7"));
+        assertTrue(codeMap.get("jimbo").contains("2"));
+        assertFalse(codeMap.get("skadoosh").contains("3"));
+        assertFalse(codeMap.get("jimbo").contains("5"));
+    }
+
+    @Test
+    public void getClient() {
+        OkHttpClient hello = new OkHttpClient.Builder().build();
+        OkHttpClient goodbye = Terminology.getClient();
+        assertEquals(hello.getClass(), goodbye.getClass());
+    }
+
+    @Test
+    public void getClient1() {
+        OkHttpClient client = Terminology.getClient("www.hello.com", 8080);
+        assertTrue(client.proxy().address().toString().contains("www.hello.com"));
+        assertTrue(client.proxy().address().toString().contains("8080"));
+    }
+
+    @Test
+    public void getAuthToken() {
+        OkHttpClient hello;
+        String PORT = Config.get("terminology.PORT");
+        String HOSTNAME = Config.get("terminology.PROXY_HOSTNAME");
+        if(HOSTNAME!=null & PORT!=null){
+            hello = new OkHttpClient.Builder()
+                    .proxy(new Proxy(Proxy.Type.HTTP,
+                            new InetSocketAddress(HOSTNAME, Integer.parseInt(PORT))))
+                    .build();
+        }else{
+            hello = new OkHttpClient.Builder()
+                    .build();
+        }
+        if(Config.get("VSAC_USER")!=null & Config.get("VSAC_PASS")!=null){
+
+            String authToken = Terminology.getAuthToken(hello);
+            assertTrue(authToken.length() > 10);
+            assertNotNull(authToken);
+        }
+
+
+    }
+
+    @Test
+    public void getServiceTicket() {
+        if(session.getAuthToken()!=null){
+            String token = Terminology.getServiceTicket(session.getAuthToken(), session.getSessionClient());
+            assertTrue(token.length() > 10);
+            assertNotNull(token);
+        }
+    }
+
+
+    // These functions aren't used yet
+
+//    @Test
+//    public void makeGenericPost() {
+//    }
+//
+//    @Test
+//    public void makeGenericGet() {
+//    }
+
+
+    @Test
+    public void getValueSet1() throws IOException {
+        OkHttpClient hello = session.getSessionClient();
+        if(session.getAuthToken()!=null){
+            String token = Terminology.getServiceTicket(session.getAuthToken(), session.getSessionClient());
+
+            String sap = Objects.requireNonNull(Terminology.getValueSet(hello, "2.16.840.1.113883.3.464.1003.103.12.1020", token).body()).string();
+            assertTrue(sap.contains("190330002"));
+            assertTrue(sap.contains("250.23"));
+            assertTrue(sap.contains("E10.3219"));
+        }
+    }
+
+    @Test
+    public void makeRequest() {
+        String url = "http://echo.jsontest.com/title/ipsum/content/blah";
+        Request request = Terminology.makeRequest(url);
+        assertEquals(request.url().toString(), url);
+        try {
+            Response response = session.getSessionClient().newCall(request).execute();
+
+            assertTrue(Objects.requireNonNull(response.body()).string().contains("ipsum"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Test
+    public void makeRequest1() {
+        FormBody formBody = new FormBody.Builder().add("json", "{1:2}").build();
+        Request request = Terminology.makeRequest("http://validate.jsontest.com/", formBody);
+        try {
+            Response response = session.getSessionClient().newCall(request).execute();
+            String body = Objects.requireNonNull(response.body()).string();
+            assertTrue(body.contains("validate"));
+            assertTrue(body.contains("true"));
+            assertFalse(body.contains("error"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void makeRequestBody() {
+        Map<String, String> alpha = new HashMap<>();
+        alpha.put("yes", "no");
+        alpha.put("thirst", "quenched");
+        RequestBody requestBody = Terminology.makeRequestBody(alpha);
+        Request request = new Request.Builder().url("http://httpbin.org/post").post(requestBody).build();
+        try {
+            Response response = session.getSessionClient().newCall(request).execute();
+            String body = Objects.requireNonNull(response.body()).string();
+            assertTrue(body.contains("thirst"));
+            assertTrue(body.contains("quenched"));
+            assertFalse(body.contains("error"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
