9aa9d03b1165c500778abd60bf05b6f2351e4c77
Lindsay Ambrosino <laambrosino@gmail.com>
2018-07-24 07:51:32 -0400

    fixed String equality testing

--- src/main/java/org/mitre/synthea/engine/Logic.java
+++ src/main/java/org/mitre/synthea/engine/Logic.java
@@ -196,9 +196,9 @@ public abstract class Logic {
       if (valueCode != null) {
         value = valueCode;
       } 
-      if (operator == "is nil") {
+      if (operator.equals("is nil")) {
         return observation == null;
-      } else if (operator == "is not nil") {
+      } else if (operator.equals("is not nil") {
         return observation != null;
       } else {
         return Utilities.compare(observation.value, this.value, operator);
