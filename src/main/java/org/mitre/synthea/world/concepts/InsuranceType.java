8549a88a8193899e45d20959d2286b40e659e29d
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-09-06 17:06:25 -0400

    added basic insurance class which will hold information about the insurance types

---
+++ src/main/java/org/mitre/synthea/world/concepts/InsuranceType.java
@@ -0,0 +1,14 @@
+package org.mitre.synthea.world.concepts;
+
+public class InsuranceType {
+
+  private String insuranceName;
+
+  public void setName(String name) {
+    insuranceName = name;
+  }
+
+  public String getInsuranceName() {
+    return insuranceName;
+  }
+}
