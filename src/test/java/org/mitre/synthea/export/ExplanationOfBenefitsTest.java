e69b99ca547b9d8dac0a4718669058b4dea383b2
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-09-11 16:52:22 -0400

    Fixed the validator not using the structure definition

--- src/test/java/org/mitre/synthea/export/ExplanationOfBenefitsTest.java
+++ src/test/java/org/mitre/synthea/export/ExplanationOfBenefitsTest.java
@@ -1,9 +1,9 @@
 package org.mitre.synthea.export;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.validation.ValidationResult;
 import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Meta;
 import org.junit.Test;
 import org.mitre.synthea.TestHelper;
 import org.mitre.synthea.engine.Generator;
@@ -13,12 +13,10 @@ import org.mitre.synthea.world.agents.Person;
 public class ExplanationOfBenefitsTest {
 
   @Test
-  public void testEobConformance() throws Exception {
+  public void testEobConformance() {
     FhirContext ctx = FhirContext.forDstu3();
-    IParser parser = ctx.newJsonParser().setPrettyPrint(true);
 
     ValidationResources validator = new ValidationResources();
-
 
     int numberOfPeople = 1;
     Generator generator = new Generator(numberOfPeople);
@@ -31,16 +29,19 @@ public class ExplanationOfBenefitsTest {
       FhirDstu2.TRANSACTION_BUNDLE = person.random.nextBoolean();
       String fhirJson = FhirStu3.convertToFHIR(person, System.currentTimeMillis());
       Bundle resource = (Bundle) ctx.newJsonParser().parseResource(fhirJson);
-      for (Bundle.BundleEntryComponent bec : resource.getEntry()) {
-        if (bec.getResource().fhirType() == "ExplanationOfBenefit") {
-          ValidationResult result = validator.validate(bec.getResource());
-          System.out.println(result);
 
+      for (Bundle.BundleEntryComponent bec : resource.getEntry()) {
+        if (bec.getResource().fhirType().equals("ExplanationOfBenefit")) {
+          ValidationResult resultNormal = validator.validate(bec.getResource());
+          bec.getResource().setMeta(new Meta().addProfile("https://bluebutton.cms.gov/assets/ig/StructureDefinition-bluebutton-inpatient-claim"));
+          ValidationResult resultBlueButton = validator.validate(bec.getResource());
+
+          System.out.println("Against BlueButton");
+          System.out.println(resultBlueButton);
+          System.out.println("Against FHIR");
+          System.out.println(resultNormal);
         }
       }
-
-
-
     }
   }
 }
