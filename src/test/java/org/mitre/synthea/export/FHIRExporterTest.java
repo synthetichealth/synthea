de34fb6525b37d07c09819a5bbd499d4ca4d03bb
Geoff Low <glow@mdsol.com>
2018-09-13 13:38:15 +0100

    Code formatting Added Before and After for setting environment Moved environment setting to Before and After

--- src/test/java/org/mitre/synthea/export/FHIRExporterTest.java
+++ src/test/java/org/mitre/synthea/export/FHIRExporterTest.java
@@ -18,6 +18,8 @@ import org.hl7.fhir.dstu3.model.Condition;
 import org.hl7.fhir.dstu3.model.Condition.ConditionClinicalStatus;
 import org.hl7.fhir.dstu3.model.Observation;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -30,114 +32,125 @@ import org.mitre.synthea.world.agents.Person;
  * Uses HAPI FHIR project to validate FHIR export. http://hapifhir.io/doc_validation.html
  */
 public class FHIRExporterTest {
-  /**
-   * Temporary folder for any exported files, guaranteed to be deleted at the end of the test.
-   */
-  @Rule
-  public TemporaryFolder tempFolder = new TemporaryFolder();
-  
-  @Test
-  public void testFHIRExport() throws Exception {
-    Config.set("exporter.baseDirectory", tempFolder.newFolder().toString());
-    
-    FhirContext ctx = FhirContext.forDstu3();
-    IParser parser = ctx.newJsonParser().setPrettyPrint(true);
+    /**
+     * Temporary folder for any exported files, guaranteed to be deleted at the end of the test.
+     */
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
 
-    FhirValidator validator = ctx.newValidator();
-    validator.setValidateAgainstStandardSchema(true);
-    validator.setValidateAgainstStandardSchematron(true);
-
-    List<String> validationErrors = new ArrayList<String>();
-
-    int numberOfPeople = 10;
-    Generator generator = new Generator(numberOfPeople);
-    for (int i = 0; i < numberOfPeople; i++) {
-      int x = validationErrors.size();
-      TestHelper.exportOff();
-      Person person = generator.generatePerson(i);
-      Config.set("exporter.fhir.export", "true");
-      Config.set("exporter.fhir.use_shr_extensions", "true");
-      FhirDstu2.TRANSACTION_BUNDLE = person.random.nextBoolean();
-      String fhirJson = FhirStu3.convertToFHIR(person, System.currentTimeMillis());
-      IBaseResource resource = ctx.newJsonParser().parseResource(fhirJson);
-      ValidationResult result = validator.validateWithResult(resource);
-      if (result.isSuccessful() == false) {
-        // If the validation failed, let's crack open the Bundle and validate
-        // each individual entry.resource to get context-sensitive error
-        // messages...
-        Bundle bundle = parser.parseResource(Bundle.class, fhirJson);
-        for (BundleEntryComponent entry : bundle.getEntry()) {
-          ValidationResult eresult = validator.validateWithResult(entry.getResource());
-          if (eresult.isSuccessful() == false) {
-            for (SingleValidationMessage emessage : eresult.getMessages()) {
-              if (emessage.getSeverity() == ResultSeverityEnum.ERROR
-                  || emessage.getSeverity() == ResultSeverityEnum.FATAL) {
-                boolean valid = false;
-                /*
-                 * There are a few bugs in the FHIR schematron files that are distributed with HAPI
-                 * 3.0.0 (these are fixed in the latest `master` branch), specifically with XPath
-                 * expressions.
-                 *
-                 * Two of these bugs are related to the FHIR Invariant rules obs-7 and con-4, which
-                 * have XPath expressions that incorrectly raise errors on validation.
-                 */
-                if (emessage.getMessage().contains("Message=obs-7")) {
-                  /*
-                   * The obs-7 invariant basically says that Observations should have values, unless
-                   * they are made of components. This test replaces an invalid XPath expression
-                   * that was causing correct instances to fail validation.
-                   */
-                  valid = validateObs7((Observation) entry.getResource());
-                } else if (emessage.getMessage().contains("Message=con-4")) {
-                  /*
-                   * The con-4 invariant says "If condition is abated, then clinicalStatus must be
-                   * either inactive, resolved, or remission" which is very clear and sensical.
-                   * However, the XPath expression does not evaluate correctly for valid instances,
-                   * so we must manually validate.
-                   */
-                  valid = validateCon4((Condition) entry.getResource());
-                }
-                if (!valid) {
-                  System.out.println(parser.encodeResourceToString(entry.getResource()));
-                  System.out.println("ERROR: " + emessage.getMessage());
-                  validationErrors.add(emessage.getMessage());
-                }
-              }
-            }
-          }
-        }
-      }
-      int y = validationErrors.size();
-      if (x != y) {
-        Exporter.export(person, System.currentTimeMillis());
-      }
+    @Before
+    public void setUp(){
+        TestHelper.exportOff();
+        Config.set("exporter.fhir.export", "true");
+        Config.set("exporter.fhir.use_shr_extensions", "true");
     }
 
-    assertEquals(0, validationErrors.size());
-  }
+    @After
+    public void tearDown(){
+        Config.remove("exporter.baseDirectory");
+        Config.remove("exporter.fhir.export");
+        Config.remove("exporter.fhir.use_shr_extensions");
+    }
 
-  /**
-   * The obs-7 invariant basically says that Observations should have values, unless they are made
-   * of components. This test replaces an invalid XPath expression that was causing correct
-   * instances to fail validation.
-   * 
-   * @param obs The observation.
-   * @return true on valid, otherwise false.
-   */
-  public static boolean validateObs7(Observation obs) {
-    return (obs.hasComponent() && !obs.hasValue()) || (obs.hasValue() && !obs.hasComponent());
-  }
+    @Test
+    public void testFHIRExport() throws Exception {
+        Config.set("exporter.baseDirectory", tempFolder.newFolder().toString());
 
-  /**
-   * The con-4 invariant says "If condition is abated, then clinicalStatus must be either inactive,
-   * resolved, or remission" which is very clear and sensical. However, the XPath expression does
-   * not evaluate correctly for valid instances, so we must manually validate.
-   * 
-   * @param con The condition.
-   * @return true on valid, otherwise false.
-   */
-  public static boolean validateCon4(Condition con) {
-    return (con.hasAbatement() && con.getClinicalStatus() != ConditionClinicalStatus.ACTIVE)
-        || !con.hasAbatement();
-  }
+        FhirContext ctx = FhirContext.forDstu3();
+        IParser parser = ctx.newJsonParser().setPrettyPrint(true);
+
+        FhirValidator validator = ctx.newValidator();
+        validator.setValidateAgainstStandardSchema(true);
+        validator.setValidateAgainstStandardSchematron(true);
+
+        List<String> validationErrors = new ArrayList<String>();
+
+        int numberOfPeople = 10;
+        Generator generator = new Generator(numberOfPeople);
+        for (int i = 0; i < numberOfPeople; i++) {
+            int x = validationErrors.size();
+            Person person = generator.generatePerson(i);
+            FhirStu3.TRANSACTION_BUNDLE = person.random.nextBoolean();
+            String fhirJson = FhirStu3.convertToFHIR(person, System.currentTimeMillis());
+            IBaseResource resource = ctx.newJsonParser().parseResource(fhirJson);
+            ValidationResult result = validator.validateWithResult(resource);
+            if (!result.isSuccessful()) {
+                // If the validation failed, let's crack open the Bundle and validate
+                // each individual entry.resource to get context-sensitive error
+                // messages...
+                Bundle bundle = parser.parseResource(Bundle.class, fhirJson);
+                for (BundleEntryComponent entry : bundle.getEntry()) {
+                    ValidationResult eresult = validator.validateWithResult(entry.getResource());
+                    if (!eresult.isSuccessful()) {
+                        for (SingleValidationMessage emessage : eresult.getMessages()) {
+                            if (emessage.getSeverity() == ResultSeverityEnum.ERROR
+                                    || emessage.getSeverity() == ResultSeverityEnum.FATAL) {
+                                boolean valid = false;
+                                /*
+                                 * There are a few bugs in the FHIR schematron files that are distributed with HAPI
+                                 * 3.0.0 (these are fixed in the latest `master` branch), specifically with XPath
+                                 * expressions.
+                                 *
+                                 * Two of these bugs are related to the FHIR Invariant rules obs-7 and con-4, which
+                                 * have XPath expressions that incorrectly raise errors on validation.
+                                 */
+                                if (emessage.getMessage().contains("@ Observation obs-7")) {
+                                    /*
+                                     * The obs-7 invariant basically says that Observations should have values, unless
+                                     * they are made of components. This test replaces an invalid XPath expression
+                                     * that was causing correct instances to fail validation.
+                                     */
+                                    valid = validateObs7((Observation) entry.getResource());
+                                } else if (emessage.getMessage().contains("@ Condition con-4")) {
+                                    /*
+                                     * The con-4 invariant says "If condition is abated, then clinicalStatus must be
+                                     * either inactive, resolved, or remission" which is very clear and sensical.
+                                     * However, the XPath expression does not evaluate correctly for valid instances,
+                                     * so we must manually validate.
+                                     */
+                                    valid = validateCon4((Condition) entry.getResource());
+                                }
+                                if (!valid) {
+                                    System.out.println(parser.encodeResourceToString(entry.getResource()));
+                                    System.out.println("ERROR: " + emessage.getMessage());
+                                    validationErrors.add(emessage.getMessage());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            int y = validationErrors.size();
+            if (x != y) {
+                Exporter.export(person, System.currentTimeMillis());
+            }
+        }
+
+        assertEquals(0, validationErrors.size());
+    }
+
+    /**
+     * The obs-7 invariant basically says that Observations should have values, unless they are made
+     * of components. This test replaces an invalid XPath expression that was causing correct
+     * instances to fail validation.
+     *
+     * @param obs The observation.
+     * @return true on valid, otherwise false.
+     */
+    private static boolean validateObs7(Observation obs) {
+        return (obs.hasComponent() && !obs.hasValue()) || (obs.hasValue() && !obs.hasComponent());
+    }
+
+    /**
+     * The con-4 invariant says "If condition is abated, then clinicalStatus must be either inactive,
+     * resolved, or remission" which is very clear and sensical. However, the XPath expression does
+     * not evaluate correctly for valid instances, so we must manually validate.
+     *
+     * @param con The condition.
+     * @return true on valid, otherwise false.
+     */
+    private static boolean validateCon4(Condition con) {
+        return (con.hasAbatement() && con.getClinicalStatus() != ConditionClinicalStatus.ACTIVE)
+                || !con.hasAbatement();
+    }
 }
