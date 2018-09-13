617d4c5ab9c5131f066df3be6cf546230f037d60
Geoff Low <glow@mdsol.com>
2018-09-13 17:11:22 +0100

    Fixed formatting issues (indents, docs and imports)

--- src/main/java/org/mitre/synthea/helpers/Config.java
+++ src/main/java/org/mitre/synthea/helpers/Config.java
@@ -9,86 +9,86 @@ import java.util.Properties;
 import java.util.Set;
 
 public abstract class Config {
-    private static Properties properties = new Properties();
+  private static Properties properties = new Properties();
 
-    static {
-        try {
-            load(Config.class.getResourceAsStream("/synthea.properties"));
-        } catch (IOException e) {
-            System.err.println("Unable to load default properties file");
-            e.printStackTrace();
-        }
+  static {
+    try {
+      load(Config.class.getResourceAsStream("/synthea.properties"));
+    } catch (IOException e) {
+      System.err.println("Unable to load default properties file");
+      e.printStackTrace();
     }
+  }
 
-    /**
-     * Load properties from a file.
-     */
-    public static void load(File propsFile) throws FileNotFoundException, IOException {
-        properties.load(new FileReader(propsFile));
-    }
+  /**
+   * Load properties from a file.
+   */
+  public static void load(File propsFile) throws FileNotFoundException, IOException {
+    properties.load(new FileReader(propsFile));
+  }
 
-    /**
-     * Load properties from an input stream. (ex, when running inside a JAR)
-     */
-    public static void load(InputStream stream) throws IOException {
-        properties.load(stream);
-    }
+  /**
+   * Load properties from an input stream. (ex, when running inside a JAR)
+   */
+  public static void load(InputStream stream) throws IOException {
+    properties.load(stream);
+  }
 
-    /**
-     * Get a named property.
-     *
-     * @param key property name
-     * @return value for the property, or null if not found
-     */
-    public static String get(String key) {
-        return properties.getProperty(key);
-    }
+  /**
+   * Get a named property.
+   *
+   * @param key property name
+   * @return value for the property, or null if not found
+   */
+  public static String get(String key) {
+    return properties.getProperty(key);
+  }
 
-    /**
-     * Get a named property, or the default value if not found.
-     *
-     * @param key          property name
-     * @param defaultValue value to return if the property is not found in the list
-     * @return value for the property, or defaultValue if not found
-     */
-    public static String get(String key, String defaultValue) {
-        return properties.getProperty(key, defaultValue);
-    }
+  /**
+   * Get a named property, or the default value if not found.
+   *
+   * @param key          property name
+   * @param defaultValue value to return if the property is not found in the list
+   * @return value for the property, or defaultValue if not found
+   */
+  public static String get(String key, String defaultValue) {
+    return properties.getProperty(key, defaultValue);
+  }
 
-    /**
-     * Manually set a property.
-     *
-     * @param key   property name
-     * @param value property value
-     */
-    public static void set(String key, String value) {
-        properties.setProperty(key, value);
-    }
+  /**
+   * Manually set a property.
+   *
+   * @param key   property name
+   * @param value property value
+   */
+  public static void set(String key, String value) {
+    properties.setProperty(key, value);
+  }
 
-    /**
-     * Get a set of the names for all properties in the config file.
-     *
-     * <p>Returns a set of keys in this property list where the key and its corresponding value are
-     * strings, including distinct keys in the default property list if a key of the same name has not
-     * already been found from the main properties list. Properties whose key or value is not of type
-     * String are omitted. The returned set is not backed by the Properties object. Changes to this
-     * Properties are not reflected in the set, or vice versa.
-     *
-     * @return Set of property key names
-     */
-    public static Set<String> allPropertyNames() {
-        return properties.stringPropertyNames();
-    }
+  /**
+   * Get a set of the names for all properties in the config file.
+   *
+   * <p>Returns a set of keys in this property list where the key and its corresponding value are
+   * strings, including distinct keys in the default property list if a key of the same name has not
+   * already been found from the main properties list. Properties whose key or value is not of type
+   * String are omitted. The returned set is not backed by the Properties object. Changes to this
+   * Properties are not reflected in the set, or vice versa.
+   *
+   * @return Set of property key names
+   */
+  public static Set<String> allPropertyNames() {
+    return properties.stringPropertyNames();
+  }
 
-    /**
-     * Unset a configuration value
-     *
-     * @param key property name
-     */
-    public static void remove(String key) {
-        if (properties.stringPropertyNames().contains(key)) {
-            properties.remove(key);
-        }
+  /**
+   * Unset a configuration value
+   *
+   * @param key property name
+   */
+  public static void remove(String key) {
+    if (properties.stringPropertyNames().contains(key)) {
+      properties.remove(key);
     }
+  }
 
 }
