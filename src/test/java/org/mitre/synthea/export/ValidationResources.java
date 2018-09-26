5b2df0de633911b92fabd7b9f70f8222e8847728
Jason Walonoski <jwalonoski@mitre.org>
2018-09-26 14:40:25 -0400

    Complete validation test classes.

--- src/test/java/org/mitre/synthea/export/ValidationResources.java
+++ src/test/java/org/mitre/synthea/export/ValidationResources.java
@@ -1,8 +1,6 @@
 package org.mitre.synthea.export;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.parser.IParser;
-import ca.uhn.fhir.parser.StrictErrorHandler;
 import ca.uhn.fhir.validation.FhirValidator;
 import ca.uhn.fhir.validation.SingleValidationMessage;
 import ca.uhn.fhir.validation.ValidationResult;
@@ -11,32 +9,24 @@ import org.hl7.fhir.dstu3.hapi.validation.ValidationSupportChain;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.dstu3.hapi.ctx.IValidationSupport;
 import org.hl7.fhir.dstu3.hapi.validation.FhirInstanceValidator;
-import org.hl7.fhir.dstu3.model.StructureDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
+/**
+ * ValidationResources loads validation resources (e.g. StructureDefinitions, ValueSets,
+ * etc) and validates Resources conforming to those profiles using the validate method.
+ */
 public class ValidationResources {
   private FhirContext ctx;
   private FhirValidator validator;
   private FhirInstanceValidator instanceValidator;
   static final Logger logger = LoggerFactory.getLogger(ValidationResources.class);
 
-
   /**
-   * Constructor for the class that creates the context and validator for usage by the
-   * rest of the program.
+   * Create FHIR context, validator, and validation support.
    */
   public ValidationResources() {
-
-    //Only support for dstu3 for now
+    // Only support for dstu3 for now
     ctx = FhirContext.forDstu3();
     validator = ctx.newValidator();
 
@@ -46,73 +36,35 @@ public class ValidationResources {
         new DefaultProfileValidationSupport());
     instanceValidator.setValidationSupport(support);
     validator.registerValidatorModule(instanceValidator);
-
   }
 
   /**
-   * Loads the structure definitions from the given directory.
-   * @param rootDir the directory to load structure definitions from
-   * @return a list of structure definitions
-   */
-  public static List<StructureDefinition> loadFromDirectory(String rootDir) {
-
-    IParser jsonParser = FhirContext.forDstu3().newJsonParser();
-    jsonParser.setParserErrorHandler(new StrictErrorHandler());
-    List<StructureDefinition> definitions = new ArrayList<>();
-
-    File[] profiles =
-        new File(ValidationResources.class.getClassLoader()
-            .getResource(rootDir)
-            .getFile())
-            .listFiles();
-
-    Arrays.asList(profiles).forEach(f -> {
-      try {
-        StructureDefinition sd = jsonParser.parseResource(StructureDefinition.class,
-            new FileReader(f));
-        definitions.add(sd);
-      } catch (FileNotFoundException e) {
-        throw new RuntimeException(e);
-      }
-    });
-
-    return definitions;
-  }
-
-  /**
-   * Runs the validator on a given resource and deals with response.
+   * Runs validation on a given resource, logs the results, and returns the response.
    * @param theResource the resource to be validated
-   * @return whether the validation was successful or unsuccessful
+   * @return ValidationResult
    */
   public ValidationResult validate(IBaseResource theResource) {
     ValidationResult result = validator.validateWithResult(theResource);
-
-
     // Do we have any errors or fatal errors?
-
     // Show the issues
     for (SingleValidationMessage next : result.getMessages()) {
       switch (next.getSeverity()) {
-        case ERROR:
-          logger.error(next.getLocationString() + " - " + next.getMessage());
-          break;
-        case INFORMATION:
-          logger.info(next.getLocationString() + " - " + next.getMessage());
-          break;
-        case WARNING:
-          logger.warn(next.getLocationString() + " - " + next.getMessage());
-          break;
-        case FATAL:
-          logger.error(next.getLocationString() + " - " + next.getMessage());
-          break;
-        default:
-          logger.debug(next.getLocationString() + " - " + next.getMessage());
+      case ERROR:
+        logger.error(next.getLocationString() + " - " + next.getMessage());
+        break;
+      case INFORMATION:
+        logger.info(next.getLocationString() + " - " + next.getMessage());
+        break;
+      case WARNING:
+        logger.warn(next.getLocationString() + " - " + next.getMessage());
+        break;
+      case FATAL:
+        logger.error(next.getLocationString() + " - " + next.getMessage());
+        break;
+      default:
+        logger.debug(next.getLocationString() + " - " + next.getMessage());
       }
     }
-
     return result;
-
   }
-
-
 }
--- src/test/java/org/mitre/synthea/export/ValidationSupport.java
+++ src/test/java/org/mitre/synthea/export/ValidationSupport.java
@@ -1,62 +1,122 @@
 package org.mitre.synthea.export;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.parser.StrictErrorHandler;
+
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.utilities.validation.ValidationMessage.IssueSeverity;
 import org.hl7.fhir.dstu3.hapi.ctx.IValidationSupport;
 import org.hl7.fhir.dstu3.model.CodeSystem;
+import org.hl7.fhir.dstu3.model.CodeSystem.ConceptDefinitionComponent;
 import org.hl7.fhir.dstu3.model.StructureDefinition;
 import org.hl7.fhir.dstu3.model.ValueSet;
 import org.hl7.fhir.dstu3.model.ValueSet.ConceptSetComponent;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-
+/**
+ * ValidationSupport provides implementation guide profiles (i.e. StructureDefinitions)
+ * to the FHIR validation process. This class does not provide ValueSet expansion.
+ */
 public class ValidationSupport implements IValidationSupport {
   private static String profileDir = "structureDefinitions";
 
-  private final HashMap<String, StructureDefinition> definitionsMap;
-  private final List<StructureDefinition> definitions;
-
+  private List<IBaseResource> resources;
+  private Map<String, IBaseResource> resourcesMap;
+  private List<StructureDefinition> definitions;
+  private Map<String, StructureDefinition> definitionsMap;
+  private Map<String, CodeSystem> codeSystemMap;
 
   /**
-   * Defines the custom validation support for DaVinci work.  Passes the structure definitions
-   * to the validator to provide custom validations.
+   * Defines the custom validation support for various implementation guides.
    */
   public ValidationSupport() {
+    resources = new ArrayList<IBaseResource>();
+    resourcesMap = new HashMap<String, IBaseResource>();
+    definitions = new ArrayList<StructureDefinition>();
+    definitionsMap = new HashMap<String, StructureDefinition>();
+    codeSystemMap = new HashMap<String, CodeSystem>();
 
-    definitions = ValidationResources.loadFromDirectory(profileDir);
-    definitionsMap = new HashMap<>();
-    definitions.forEach(def -> definitionsMap.put(def.getUrl(), def));
+    try {
+      loadFromDirectory(profileDir);
+    } catch (Throwable t) {
+      throw new RuntimeException(t);
+    }
+  }
+
+  /**
+   * Loads the structure definitions from the given directory.
+   * @param rootDir the directory to load structure definitions from
+   * @return a list of structure definitions
+   * @throws Throwable
+   */
+  private void loadFromDirectory(String rootDir) throws Throwable {
+
+    IParser jsonParser = FhirContext.forDstu3().newJsonParser();
+    jsonParser.setParserErrorHandler(new StrictErrorHandler());
+
+    URL profilesFolder = ClassLoader.getSystemClassLoader().getResource(rootDir);
+    Path path = Paths.get(profilesFolder.toURI());
+    Files.walk(path, Integer.MAX_VALUE).filter(Files::isReadable).filter(Files::isRegularFile)
+        .filter(p -> p.toString().endsWith(".json")).forEach(f -> {
+      try {
+        IBaseResource resource = jsonParser.parseResource(new FileReader(f.toFile()));
+        resources.add(resource);
+        if (resource instanceof CodeSystem) {
+          CodeSystem cs = (CodeSystem) resource;
+          resourcesMap.put(cs.getUrl(), cs);
+          codeSystemMap.put(cs.getUrl(), cs);
+        } else if (resource instanceof ValueSet) {
+          ValueSet vs = (ValueSet) resource;
+          resourcesMap.put(vs.getUrl(), vs);
+        } else if (resource instanceof StructureDefinition) {
+          StructureDefinition sd = (StructureDefinition) resource;
+          resourcesMap.put(sd.getUrl(), sd);
+          definitions.add(sd);
+          definitionsMap.put(sd.getUrl(), sd);
+        }
+      } catch (FileNotFoundException e) {
+        throw new RuntimeException(e);
+      }
+    });
   }
 
   @Override
   public ValueSet.ValueSetExpansionComponent expandValueSet(FhirContext theContext,
-                                                            ConceptSetComponent theInclude) {
+      ConceptSetComponent theInclude) {
     return null;
   }
 
   @Override
   public List<IBaseResource> fetchAllConformanceResources(FhirContext theContext) {
-
-    return null;
+    return resources;
   }
 
   @Override
   public List<StructureDefinition> fetchAllStructureDefinitions(FhirContext theContext) {
-
     return definitions;
   }
 
   @Override
   public CodeSystem fetchCodeSystem(FhirContext theContext, String theSystem) {
-    return null;
+    return codeSystemMap.get(theSystem);
   }
 
+  @SuppressWarnings("unchecked")
   @Override
   public <T extends IBaseResource> T fetchResource(FhirContext theContext,
-                                                   Class<T> theClass, String theUri) {
-    return (T) definitionsMap.get(theUri);
+      Class<T> theClass, String theUri) {
+    return (T) resourcesMap.get(theUri);
   }
 
   @Override
@@ -66,15 +126,38 @@ public class ValidationSupport implements IValidationSupport {
 
   @Override
   public boolean isCodeSystemSupported(FhirContext theContext, String theSystem) {
-    return false;
+    return codeSystemMap.containsKey(theSystem);
   }
 
   @Override
   public CodeValidationResult validateCode(FhirContext theContext, String theCodeSystem,
-                                           String theCode, String theDisplay) {
-    return null;
+      String theCode, String theDisplay) {
+    IssueSeverity severity = IssueSeverity.WARNING;
+    String message = "Unsupported CodeSystem";
+
+    if (isCodeSystemSupported(theContext, theCodeSystem)) {
+      severity = IssueSeverity.ERROR;
+      message = "Code not found";
+
+      CodeSystem cs = codeSystemMap.get(theCodeSystem);
+      for (ConceptDefinitionComponent def : cs.getConcept()) {
+        if (def.getCode().equals(theCode)) {
+          if (def.getDisplay() != null && theDisplay != null) {
+            if (def.getDisplay().equals(theDisplay)) {
+              severity = IssueSeverity.INFORMATION;
+              message = "Validated Successfully";
+            } else {
+              severity = IssueSeverity.WARNING;
+              message = "Validated Code; Display mismatch";
+            }
+          } else {
+            severity = IssueSeverity.WARNING;
+            message = "Validated Code; No display";
+          }
+        }
+      }
+    }
+
+    return new CodeValidationResult(severity, message);
   }
 }
-
-
-//Definitions list gotten from code above
