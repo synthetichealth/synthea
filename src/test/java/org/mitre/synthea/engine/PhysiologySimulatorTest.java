96c48fe39d3c3418e955f23282caaf7d7ab4e997
Ryan Sivek <9834834+nanowizard@users.noreply.github.com>
2019-07-26 20:51:35 -0400

    add tests for PhysiologySimulator (not yet passing)

--- src/test/java/org/mitre/synthea/engine/PhysiologySimulatorTest.java
+++ src/test/java/org/mitre/synthea/engine/PhysiologySimulatorTest.java
@@ -1,5 +1,6 @@
 package org.mitre.synthea.engine;
 
+import com.google.common.collect.Lists;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -10,104 +11,74 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.math.ode.DerivativeException;
 
 import org.junit.Test;
 import org.powermock.reflect.Whitebox;
+import org.simulator.math.odes.MultiTable;
 
-public class ModuleTest {
-
-  @Test
-  public void getModules() {
-    List<Module> allModules = Module.getModules();
-    List<Module> someModules = Module.getModules(path -> path.contains("ti"));
-    
-    assertTrue(allModules.containsAll(someModules));
-    assertFalse(someModules.containsAll(allModules));
-    assertTrue(allModules.size() > someModules.size());
-    assertTrue(someModules.size() > 0);
-
-    assertTrue(allModules.stream().anyMatch(filterOnModuleName("COPD")));
-    assertTrue(someModules.stream().anyMatch(filterOnModuleName("Dermatitis")));
-    assertFalse(someModules.stream().anyMatch(filterOnModuleName("COPD")));
-  }
+public class PhysiologySimulatorTest {
 
   @Test
-  public void getModuleByPath() {
-    Module module = Module.getModuleByPath("copd");
-    assertNotNull(module);
-    assertEquals("COPD Module", module.name);
-  }
-
-  @Test
-  public void getModuleByPath_badModule() throws Exception {
-    IOException fault = new IOException("Deliberate failure");
-    try (FaultyModuleScope ignore = injectFaultIntoModuleLoad("bad_module", fault)) {
-      Module.getModuleByPath("bad_module");
-      fail("Expected getModuleByPath() to fail with a RuntimeException");
-    } catch (RuntimeException e) {
-      assertSame(fault, e.getCause());
+  public void testCVSSimulation() {
+    try {
+      
+      PhysiologySimulator physio = new PhysiologySimulator("circulation/Smith2004_CVS_human.xml", "runge_kutta", 0.01, 4, 0.0);
+      
+      // Ensure we can get parameters. Check a couple
+      List<String> params = physio.getParameters();
+      assertTrue("V_lv", params.contains("V_lv"));
+      assertTrue("P_rv", params.contains("P_rv"));
+      assertTrue("period", params.contains("period"));
+      assertTrue("P_ao", params.contains("P_ao"));
+      
+      // First run with all default parameters
+      MultiTable results = physio.run(new HashMap());
+      
+      List<Double> pao = Lists.newArrayList(results.getColumn("P_ao"));
+      Double sys = Collections.max(pao);
+      Double dia = Collections.min(pao);
+      
+      System.out.println("sys 1: " + sys);
+      System.out.println("dia 1: " + dia);
+      
+      assertTrue("sys > 110", sys > 110);
+      assertTrue("sys < 120", sys < 120);
+      assertTrue("dia > 70", dia > 70);
+      assertTrue("dia < 80", dia < 80);
+      
+      Map<String,Double> inputs = new HashMap();
+      inputs.put("R_sys", 1.814);
+      
+      // Run with some inputs
+      results = physio.run(inputs);
+      
+      pao = Lists.newArrayList(results.getColumn("P_ao"));
+      sys = Collections.max(pao);
+      dia = Collections.min(pao);
+      
+      System.out.println("sys 2: " + sys);
+      System.out.println("dia 2: " + dia);
+      
+      assertEquals(results.getColumn("R_sys").getValue(0), 1.814, 0.0001);
+      
+      // Check that levels have appropriately changed
+      assertTrue("sys > 140", sys > 140);
+      assertTrue("sys < 150", sys < 150);
+      assertTrue("dia > 80", dia > 80);
+      assertTrue("dia < 90", dia < 90);
+      
+    } catch (DerivativeException ex) {
+      throw new RuntimeException(ex);
     }
-    Module.getModuleByPath("bad_module"); // should not fail now
-  }
-
-  @Test
-  public void getModuleByPath_missingModule() {
-    Module module = Module.getModuleByPath("missing_module");
-    assertNull(module);
-  }
-
-  /**
-   * Injects a fault into the lazy load of the specified module. A FaultyModuleScope object is 
-   * returned for convenience with try-with-resources to ensure the module is put back the way it
-   * was before we broke it.
-   * @param path The module to inject a fault into. It does not need to presently exist.
-   * @param fault The fault to inject.
-   * @return A FaultyModuleScope that restores the previous state for the specified module.
-   * @throws Exception If something goes terribly wrong.
-   */
-  private static FaultyModuleScope injectFaultIntoModuleLoad(String path, Exception fault)
-      throws Exception {
-    Field modulesField = Whitebox.getField(Module.class, "modules");
-    modulesField.setAccessible(true);
-    @SuppressWarnings("unchecked")
-    Map<String, Module.ModuleSupplier> modules = 
-            (Map<String, Module.ModuleSupplier>)modulesField.get(null);
-    
-    // Store the old supplier and inject our "broken" one.
-    Module.ModuleSupplier originalSupplier = modules.get(path);
-    Callable<Module> faultyCallable = () -> {
-      throw fault;
-    };
-    Module.ModuleSupplier faultySupplier = new Module.ModuleSupplier(false, path, faultyCallable);
-    modules.put(path, faultySupplier);
-    
-    // A runnable that safely puts everything back the way it was.
-    AtomicBoolean runOnce = new AtomicBoolean();
-    return () -> {
-      if (runOnce.compareAndSet(false, true)) {
-        if (originalSupplier == null) {
-          modules.remove(path, faultySupplier);
-        } else {
-          modules.replace(path, faultySupplier, originalSupplier);
-        }
-      }
-    };
-  }
-  
-  private static Predicate<Module> filterOnModuleName(String partialName) {
-    return module -> {
-      String name = module.name;
-      return name != null && name.contains(partialName);
-    };
-  }
-  
-  public interface FaultyModuleScope extends AutoCloseable {
-    @Override
-    void close();
   }
 }
