54b5c35c03262b839106fb1a4efbb5f43188da37
Andy Gregorowicz <andrewg@mitre.org>
2019-09-05 17:21:29 -0400

    WIP - assignPoint based on zip if it exists

--- src/main/java/org/mitre/synthea/world/geography/Location.java
+++ src/main/java/org/mitre/synthea/world/geography/Location.java
@@ -246,7 +246,7 @@ public class Location {
    * @param cityName Name of the city, or null to choose one randomly
    */
   public void assignPoint(Person person, String cityName) {
-    List<Place> zipsForCity = null;
+    List<Place> zipsForCity;
 
     if (cityName == null) {
       int size = zipCodes.keySet().size();
@@ -258,12 +258,19 @@ public class Location {
       zipsForCity = zipCodes.get(cityName + " Town");
     }
     
-    Place place = null;
+    Place place;
     if (zipsForCity.size() == 1) {
       place = zipsForCity.get(0);
     } else {
-      // pick a random one
-      place = zipsForCity.get(person.randInt(zipsForCity.size()));
+      String personZip = (String) person.attributes.get(Person.ZIP);
+      if (personZip == null) {
+        place = zipsForCity.get(person.randInt(zipsForCity.size()));
+      } else {
+        place = zipsForCity.stream()
+            .filter(c -> personZip.equals(c.postalCode))
+            .findFirst()
+            .orElse(zipsForCity.get(person.randInt(zipsForCity.size())));
+      }
     }
     
     if (place != null) {
