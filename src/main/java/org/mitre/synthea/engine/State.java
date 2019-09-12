95e3f6df04cf8afa4a8ccd0f025a7e66b4312e97
Ryan Sivek <9834834+nanowizard@users.noreply.github.com>
2019-09-12 10:37:17 -0600

    remove extra import

--- src/main/java/org/mitre/synthea/engine/State.java
+++ src/main/java/org/mitre/synthea/engine/State.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
