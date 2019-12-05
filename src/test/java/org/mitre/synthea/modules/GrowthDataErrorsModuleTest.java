21b14e867ae23fb223c4300c9fcc02b1826e3145
Andy Gregorowicz <andrewg@mitre.org>
2019-12-05 09:54:04 -0500

    Fixing magic strings in the tests

--- src/test/java/org/mitre/synthea/modules/GrowthDataErrorsModuleTest.java
+++ src/test/java/org/mitre/synthea/modules/GrowthDataErrorsModuleTest.java
@@ -21,18 +21,18 @@ public class GrowthDataErrorsModuleTest {
     record = new HealthRecord(new Person(1));
     HealthRecord.Encounter e = record.encounterStart(1000, HealthRecord.EncounterType.OUTPATIENT);
     // Weight Observation
-    e.addObservation(1000, "29463-7", 50d);
+    e.addObservation(1000, GrowthDataErrorsModule.WEIGHT_LOINC_CODE, 50d);
     // Height Observation
-    e.addObservation(1000, "8302-2", 150d);
-    e.addObservation(1000, "39156-5", 22.2d);
+    e.addObservation(1000, GrowthDataErrorsModule.HEIGHT_LOINC_CODE, 150d);
+    e.addObservation(1000, GrowthDataErrorsModule.BMI_LOINC_CODE, 22.2d);
     e.codes.add(new HealthRecord.Code("http://snomed.info/sct", "183452005", ""));
     first = e;
     e = record.encounterStart(100000, HealthRecord.EncounterType.OUTPATIENT);
     // Weight Observation
-    e.addObservation(100000, "29463-7", 60d);
+    e.addObservation(100000, GrowthDataErrorsModule.WEIGHT_LOINC_CODE, 60d);
     // Height Observation
-    e.addObservation(100000, "8302-2", 160d);
-    e.addObservation(100000, "39156-5", 23.4d);
+    e.addObservation(100000, GrowthDataErrorsModule.HEIGHT_LOINC_CODE, 160d);
+    e.addObservation(100000, GrowthDataErrorsModule.BMI_LOINC_CODE, 23.4d);
     e.codes.add(new HealthRecord.Code("http://snomed.info/sct", "183452005", ""));
     second = e;
   }
@@ -47,60 +47,60 @@ public class GrowthDataErrorsModuleTest {
   @Test
   public void introduceWeightUnitError() {
     GrowthDataErrorsModule.introduceWeightUnitError(first);
-    assertEquals(110.25, (Double) first.findObservation("29463-7").value, 0.1);
+    assertEquals(110.25, (Double) first.findObservation(GrowthDataErrorsModule.WEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void introduceHeightUnitError() {
     GrowthDataErrorsModule.introduceHeightUnitError(first);
-    assertEquals(59.1, (Double) first.findObservation("8302-2").value, 0.1);
+    assertEquals(59.1, (Double) first.findObservation(GrowthDataErrorsModule.HEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void introduceTransposeError() {
     GrowthDataErrorsModule.introduceTransposeError(first, "weight");
-    assertEquals(5, (Double) first.findObservation("29463-7").value, 0.1);
+    assertEquals(5, (Double) first.findObservation(GrowthDataErrorsModule.WEIGHT_LOINC_CODE).value, 0.1);
 
     GrowthDataErrorsModule.introduceTransposeError(second, "height");
-    assertEquals(106, (Double) second.findObservation("8302-2").value, 0.1);
+    assertEquals(106, (Double) second.findObservation(GrowthDataErrorsModule.HEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void introduceWeightSwitchError() {
     GrowthDataErrorsModule.introduceWeightSwitchError(first);
-    assertEquals(150, (Double) first.findObservation("29463-7").value, 0.1);
+    assertEquals(150, (Double) first.findObservation(GrowthDataErrorsModule.WEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void introduceHeightSwitchError() {
     GrowthDataErrorsModule.introduceHeightSwitchError(second);
-    assertEquals(160, (Double) second.findObservation("29463-7").value, 0.1);
+    assertEquals(160, (Double) second.findObservation(GrowthDataErrorsModule.WEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void introduceWeightExtremeError() {
     GrowthDataErrorsModule.introduceWeightExtremeError(first);
-    assertEquals(500, (Double) first.findObservation("29463-7").value, 0.1);
+    assertEquals(500, (Double) first.findObservation(GrowthDataErrorsModule.WEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void introduceHeightExtremeError() {
     GrowthDataErrorsModule.introduceHeightExtremeError(second);
-    assertEquals(1600, (Double) second.findObservation("8302-2").value, 0.1);
+    assertEquals(1600, (Double) second.findObservation(GrowthDataErrorsModule.HEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void introduceHeightAbsoluteError() {
     GrowthDataErrorsModule.introduceHeightAbsoluteError(first, new Random());
-    assertTrue((Double) first.findObservation("8302-2").value <= 147d);
-    assertTrue((Double) first.findObservation("8302-2").value >= 144d);
+    assertTrue((Double) first.findObservation(GrowthDataErrorsModule.HEIGHT_LOINC_CODE).value <= 147d);
+    assertTrue((Double) first.findObservation(GrowthDataErrorsModule.HEIGHT_LOINC_CODE).value >= 144d);
   }
 
   @Test
   public void introduceWeightDuplicateError() {
     GrowthDataErrorsModule.introduceWeightDuplicateError(first, new Random());
     long obsCount = first.observations.stream()
-        .filter(o -> o.containsCode("29463-7", "LOINC"))
+        .filter(o -> o.containsCode(GrowthDataErrorsModule.WEIGHT_LOINC_CODE, "LOINC"))
         .count();
     assertEquals(2, obsCount);
   }
@@ -109,7 +109,7 @@ public class GrowthDataErrorsModuleTest {
   public void introduceHeightDuplicateError() {
     GrowthDataErrorsModule.introduceHeightDuplicateError(first, new Random());
     long obsCount = first.observations.stream()
-        .filter(o -> o.containsCode("8302-2", "LOINC"))
+        .filter(o -> o.containsCode(GrowthDataErrorsModule.HEIGHT_LOINC_CODE, "LOINC"))
         .count();
     assertEquals(2, obsCount);
   }
@@ -117,20 +117,20 @@ public class GrowthDataErrorsModuleTest {
   @Test
   public void introduceWeightCarriedForwardError() {
     GrowthDataErrorsModule.introduceWeightCarriedForwardError(second);
-    assertEquals(50, (Double) second.findObservation("29463-7").value, 0.1);
+    assertEquals(50, (Double) second.findObservation(GrowthDataErrorsModule.WEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void introduceHeightCarriedForwardError() {
     GrowthDataErrorsModule.introduceHeightCarriedForwardError(second);
-    assertEquals(150, (Double) second.findObservation("8302-2").value, 0.1);
+    assertEquals(150, (Double) second.findObservation(GrowthDataErrorsModule.HEIGHT_LOINC_CODE).value, 0.1);
   }
 
   @Test
   public void encountersWithObservationsOfCode() {
     GrowthDataErrorsModule mod = new GrowthDataErrorsModule();
     List<HealthRecord.Encounter> es = mod.encountersWithObservationsOfCode(record.encounters,
-        "8302-2", "LOINC");
+        GrowthDataErrorsModule.HEIGHT_LOINC_CODE, "LOINC");
     assertEquals(2, es.size());
 
   }
