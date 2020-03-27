5ac8126ed72075cd4113ffc5fac877a6ea17bc83
Andy Gregorowicz <andrewg@mitre.org>
2020-03-27 16:11:03 -0400

    Updating the probabilities to look more like MA

--- src/main/resources/modules/lookup_tables/covidtimeline.py
+++ src/main/resources/modules/lookup_tables/covidtimeline.py
@@ -5,10 +5,10 @@ headers = ['time', 'Encounter for Test', 'Terminal', 'Wait Until Exposure']
 
 one_week = timedelta(days=7)
 one_second_less_than_a_week = one_week - timedelta(milliseconds=1)
-start = datetime(2019, 11, 1)
+start = datetime(2020, 2, 1)
 end = start + one_second_less_than_a_week
-start_prob = 0.01
-weekly_increase = 0.10
+probs_by_week = {0: 0.0005, 1: 0.0005, 2: 0.0005, 3: 0.0005, 4: 0.0005,
+  5: 0.01, 6: 0.02, 7: 0.2, 8: 0.4}
 max_prob = 0.75
 lucky_ones = 0.1
 week = 0
@@ -16,12 +16,9 @@ week = 0
 with open('covid19_prob.csv', 'w', newline='') as csvfile:
   writer = csv.DictWriter(csvfile, fieldnames=headers)
   writer.writeheader()
-  while end < datetime.now():
+  while end < datetime(2020, 4, 3):
     row = {}
-    prob_of_covid19 = start_prob + (week * weekly_increase)
-    if prob_of_covid19 > max_prob:
-      prob_of_covid19 = max_prob
-
+    prob_of_covid19 = probs_by_week[week]
     row['time'] = "{:0.0f}-{:0.0f}".format(start.timestamp() * 1000, end.timestamp() * 1000)
     row['Encounter for Test'] = prob_of_covid19
     row['Wait Until Exposure'] = 1 - prob_of_covid19 - lucky_ones
@@ -29,4 +26,5 @@ with open('covid19_prob.csv', 'w', newline='') as csvfile:
     writer.writerow(row)
     start += one_week
     end += one_week
+    print(end)
     week += 1
