f116de152b5db5772bd0b6847c9a460ab2f1ffbb
Dylan <dehall@mitre.org>
2020-11-10 13:40:57 -0500

    tweaks

--- src/main/java/org/mitre/synthea/modules/CardiovascularDiseaseModule.java
+++ src/main/java/org/mitre/synthea/modules/CardiovascularDiseaseModule.java
@@ -42,6 +42,7 @@ public final class CardiovascularDiseaseModule extends Module {
     // since this is intended to only be temporary
     // until we can convert this module to GMF
 
+    // TODO: make this a config parameter for which risk system we want to use
     boolean useFramingham = false;
     if (useFramingham) {
         calculateCardioRisk(person, time);
@@ -388,83 +389,100 @@ public final class CardiovascularDiseaseModule extends Module {
   
   
   /**
-   * Computes the ASCVD Risk Estimate for an individual over the next 10 years.
-   * @param patientInfo - patientInfo object from ASCVDRisk data model
-   * @returns {*} Returns the risk score or 0 if not in the appropriate age range
+   * Equation Parameters of the Pooled Cohort Equations for Estimation of 10-Year Risk of Hard ASCVD
+   * See Appendix 7, Table A  https://doi.org/10.1161/01.cir.0000437741.48606.98
+   * "N/A" becomes 0
    */
-	private static void calculateAscvdRisk(Person person, long time) {
-		int age = person.ageInYears(time);
-		String gender = (String) person.attributes.get(Person.GENDER);
-		String race = (String) person.attributes.get(Person.RACE);
-		Double sysBP = person.getVitalSign(VitalSign.SYSTOLIC_BLOOD_PRESSURE, time);
-		Double diaBP = person.getVitalSign(VitalSign.DIASTOLIC_BLOOD_PRESSURE, time);
-		Double totalChol = person.getVitalSign(VitalSign.TOTAL_CHOLESTEROL, time);
-		Double hdl = person.getVitalSign(VitalSign.HDL, time);
+	private static final double[][] ASCVD_COEFFICIENTS = {
+                          // sex:  ------women-----   ----men----
+                          // race: ---w---  --aa--  ---w--  --aa--
+/* Ln Age (y) */                 { -29.799, 17.114,  12.344, 2.469 },
+/* (Ln Age)^2 */                 { 4.884,   0,       0,      0 },
+/* Ln Total Chol */              { 13.540,  0.940,   11.853, 0.302 },
+/* Ln Age × Ln Total Chol */     { -3.114,  0,       -2.664, 0 },
+/* Ln HDL-C */                   { -13.578, -18.920, -7.990, -0.307 },
+/* Ln Age × Ln HDL-C */          { 3.149,   4.475,   1.769,  0 },
+/* Ln Treated SysBP */           { 2.019,   29.291,  1.797,  1.916 },
+/* Ln Age × Ln Treated SysBP */  { 0,       -6.432,  0,      0 },
+/* Ln Untreated SysBP */         { 1.957,   27.820,  1.764,  1.809 },
+/* Ln Age × Ln Untreat SysBP */  { 0,       -6.087,  0,      0 },
+/* Current Smoker (1=Y, 0=N) */  { 7.574,   0.691,   7.837,  0.549 },
+/* Ln Age × Current Smoker */    { -1.665,  0,       -1.795, 0 },
+/* Diabetes (1=Y, 0=N) */        { 0.661,   0.874,   0.658,  0.645 },
 
-		boolean smoker = (Boolean) person.attributes.getOrDefault(Person.SMOKER, false);
-		boolean diabetic = (Boolean) person.attributes.getOrDefault("diabetes", false);
-		boolean hypertensive = (Boolean) person.attributes.getOrDefault("hypertension", false);
-		if (sysBP == null || diaBP == null || totalChol == null) {
-			return;
-		}
-		if (age < 40 || age > 79) {
-			return;
-		}
-		double lnAge = Math.log(age);
-		double lnTotalChol = Math.log(totalChol);
-		double lnHdl = Math.log(hdl);
-		double trlnsbp = hypertensive ? Math.log(sysBP) : 0;
-		double ntlnsbp = hypertensive ? 0 : Math.log(diaBP);
-		double ageTotalChol = lnAge * lnTotalChol;
-		double ageHdl = lnAge * lnHdl;
-		double agetSbp = lnAge * trlnsbp;
-		double agentSbp = lnAge * ntlnsbp;
-		double ageSmoke = smoker ? lnAge : 0;
+/* Mean (Coefficient × Value) */ { -29.18,  86.61,   61.18,  19.54 },
+/* Baseline Survival */          { 0.9665,  0.9533,  0.9144, 0.8954 }
+	};
 
-		boolean isAA = race.equals("black");
-		boolean isMale = gender.equals("M");
-		double s010Ret = 0;
-		double mnxbRet = 0;
-		double predictRet = 0;
 
-		int smokerInt = smoker ? 1 : 0;
-		int diabeticInt = diabetic ? 1 : 0;
+  /**
+   * Calculates the 10-year ASCVD Risk Estimates.
+   */
+  private static void calculateAscvdRisk(Person person, long time) {
+	int age = person.ageInYears(time);
+	String gender = (String) person.attributes.get(Person.GENDER);
+	String race = (String) person.attributes.get(Person.RACE);
+	Double sysBP = person.getVitalSign(VitalSign.SYSTOLIC_BLOOD_PRESSURE, time);
+	Double diaBP = person.getVitalSign(VitalSign.DIASTOLIC_BLOOD_PRESSURE, time);
+	Double totalChol = person.getVitalSign(VitalSign.TOTAL_CHOLESTEROL, time);
+	Double hdl = person.getVitalSign(VitalSign.HDL, time);
 
-		if (isAA && !isMale) {
-			s010Ret = 0.95334;
-			mnxbRet = 86.6081;
-			predictRet = (17.1141 * lnAge) + (0.9396 * lnTotalChol) + (-18.9196 * lnHdl) + (4.4748 * ageHdl)
-					+ (29.2907 * trlnsbp) + (-6.4321 * agetSbp) + (27.8197 * ntlnsbp) + (-6.0873 * agentSbp)
-					+ (0.6908 * smokerInt) + (0.8738 * diabeticInt);
-		} else if (!isAA && !isMale) {
-			s010Ret = 0.96652;
-			mnxbRet = -29.1817;
-			predictRet = (-29.799 * lnAge) + (4.884 * (lnAge * lnAge)) + (13.54 * lnTotalChol) + (-3.114 * ageTotalChol)
-					+ (-13.578 * lnHdl) + (3.149 * ageHdl) + (2.019 * trlnsbp) + (1.957 * ntlnsbp) + (7.574 * smokerInt)
-					+ (-1.665 * ageSmoke) + (0.661 * diabeticInt);
-		} else if (isAA && isMale) {
-			s010Ret = 0.89536;
-			mnxbRet = 19.5425;
-			predictRet = (2.469 * lnAge) + (0.302 * lnTotalChol) + (-0.307 * lnHdl) + (1.916 * trlnsbp)
-					+ (1.809 * ntlnsbp) + (0.549 * smokerInt) + (0.645 * diabeticInt);
-		} else {
-			s010Ret = 0.91436;
-			mnxbRet = 61.1816;
-			predictRet = (12.344 * lnAge) + (11.853 * lnTotalChol) + (-2.664 * ageTotalChol) + (-7.99 * lnHdl)
-					+ (1.769 * ageHdl) + (1.797 * trlnsbp) + (1.764 * ntlnsbp) + (7.837 * smokerInt)
-					+ (-1.795 * ageSmoke) + (0.658 * diabeticInt);
-		}
-
-		double ascvdRisk = (1 - Math.pow(s010Ret, Math.exp(predictRet - mnxbRet)));
-		
-	    person.attributes.put("ascvd_risk", ascvdRisk);
-
-	    double timestepRisk = Utilities.convertRiskToTimestep(ascvdRisk, tenYearsInMS);
-	    person.attributes.put("cardio_risk", timestepRisk);
-	    
-	    double monthlyRisk = Utilities.convertRiskToTimestep(ascvdRisk, tenYearsInMS, oneMonthInMS);
-	    person.attributes.put("mi_risk", monthlyRisk);
+	boolean smoker = (Boolean) person.attributes.getOrDefault(Person.SMOKER, false);
+	boolean diabetic = (Boolean) person.attributes.getOrDefault("diabetes", false);
+	boolean hypertensive = (Boolean) person.attributes.getOrDefault("hypertension", false);
+	if (sysBP == null || diaBP == null || totalChol == null) {
+		return;
 	}
+	if (age < 40 || age > 79) {
+		return;
+	}
+	double lnAge = Math.log(age);
+	double lnTotalChol = Math.log(totalChol);
+	double lnHdl = Math.log(hdl);
+	double lnTreatedSBP = hypertensive ? Math.log(sysBP) : 0;    
+	double lnUntreatSBP = hypertensive ? 0 : Math.log(sysBP);
+    int smokerInt = smoker ? 1 : 0;
+    int diabeticInt = diabetic ? 1 : 0;
+
+    double[] values = {
+/* Ln Age (y) */                lnAge,
+/* (Ln Age)^2 */                (lnAge * lnAge),
+/* Ln Total Chol */             lnTotalChol,
+/* Ln Age × Ln Total Chol */    (lnAge * lnTotalChol),
+/* Ln HDL-C */                  lnHdl,
+/* Ln Age × Ln HDL-C */         (lnAge * lnHdl),
+/* Ln Treated SysBP */          lnTreatedSBP,
+/* Ln Age × Ln Treated SysBP */ (lnAge * lnTreatedSBP),
+/* Ln Untreated SysBP */        lnUntreatSBP,
+/* Ln Age × Ln Untreat SysBP */ (lnAge * lnUntreatSBP),
+/* Current Smoker (1=Y, 0=N) */ smokerInt,
+/* Ln Age × Current Smoker */   (lnAge * smokerInt),
+/* Diabetes (1=Y, 0=N) */       diabeticInt
+    };
+
+    int raceSexIndex = 0; // index in ASCVD_COEFFICIENTS above
+    if (gender.equals("M")) raceSexIndex += 2;
+    if (race.equals("black")) raceSexIndex += 1;
+
+
+	double raceSexMean = ASCVD_COEFFICIENTS[13][raceSexIndex];
+    double baselineSurvival = ASCVD_COEFFICIENTS[14][raceSexIndex];
+	double individualSum = 0;
+
+    for (int i = 0 ; i < 13 ; i++) {
+      individualSum += ASCVD_COEFFICIENTS[i][raceSexIndex] * values[i];
+    }
+
+	double ascvdRisk = (1 - Math.pow(baselineSurvival, Math.exp(individualSum - raceSexMean)));
+	
+    person.attributes.put("ascvd_risk", ascvdRisk);
+
+    double timestepRisk = Utilities.convertRiskToTimestep(ascvdRisk, tenYearsInMS);
+    person.attributes.put("cardio_risk", timestepRisk);
+    
+    double monthlyRisk = Utilities.convertRiskToTimestep(ascvdRisk, tenYearsInMS, oneMonthInMS);
+    person.attributes.put("mi_risk", monthlyRisk);
+  }
 
 
   /**
