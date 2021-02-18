909196deb6c18361694174e43b922e5e52a8c831
Marc Hadley <mhadley@mitre.org>
2021-02-18 08:07:45 -0500

    Fix catch blck for Java 8

--- src/test/java/org/mitre/synthea/export/BB2RIFExporterTest.java
+++ src/test/java/org/mitre/synthea/export/BB2RIFExporterTest.java
@@ -111,8 +111,8 @@ public class BB2RIFExporterTest {
       assertTrue(mapper.canMap("10509002"));
       assertEquals("J20.9", mapper.getMapped("10509002", generator));
       assertFalse(mapper.canMap("not a code"));
-    } catch (IOException e) {
+    } catch (IOException | IllegalArgumentException e) {
       // No worries. The optional mapping file is not present.
-    }      
+    }
   }
 }
