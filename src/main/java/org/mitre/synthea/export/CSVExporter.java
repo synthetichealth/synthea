f80377bc416608d7f13eb42a5e239c0e464181eb
Jason Walonoski <jwalonoski@mitre.org>
2021-03-10 17:56:52 -0500

    CSV claims and transactions skeleton.

--- src/main/java/org/mitre/synthea/export/CSVExporter.java
+++ src/main/java/org/mitre/synthea/export/CSVExporter.java
@@ -36,6 +36,7 @@ import org.mitre.synthea.world.agents.Clinician;
 import org.mitre.synthea.world.agents.Payer;
 import org.mitre.synthea.world.agents.Person;
 import org.mitre.synthea.world.agents.Provider;
+import org.mitre.synthea.world.concepts.Claim;
 import org.mitre.synthea.world.concepts.HealthRecord;
 import org.mitre.synthea.world.concepts.HealthRecord.CarePlan;
 import org.mitre.synthea.world.concepts.HealthRecord.Code;
@@ -126,6 +127,14 @@ public class CSVExporter {
    * Writer for payerTransitions.csv
    */
   private OutputStreamWriter payerTransitions;
+  /**
+   * Writer for claims.csv
+   */
+  private OutputStreamWriter claims;
+  /**
+   * Writer for claims_transactions.csv
+   */
+  private OutputStreamWriter claimsTransactions;
 
   /**
    * Charset for specifying the character set of the output files.
@@ -225,6 +234,11 @@ public class CSVExporter {
       payerTransitions = getWriter(outputDirectory, "payer_transitions.csv", append, includedFiles,
           excludedFiles);
 
+      claims = getWriter(outputDirectory, "claims.csv", append, includedFiles, excludedFiles);
+
+      claimsTransactions = getWriter(outputDirectory, "claims_transactions.csv", append,
+          includedFiles, excludedFiles);
+
       if (!append) {
         writeCSVHeaders();
       }
@@ -308,6 +322,19 @@ public class CSVExporter {
     payers.write(NEWLINE);
     payerTransitions.write("PATIENT,START_YEAR,END_YEAR,PAYER,OWNERSHIP");
     payerTransitions.write(NEWLINE);
+    claims.write("Id,PATIENTID,PROVIDERID,PRIMARYPATIENTINSURANCEID,SECONDARYPATIENTINSURANCEID,"
+        + "DEPARTMENTID,PATIENTDEPARTMENTID,DIAGNOSIS1,DIAGNOSIS2,DIAGNOSIS3,DIAGNOSIS4,"
+        + "DIAGNOSIS5,DIAGNOSIS6,DIAGNOSIS7,DIAGNOSIS8,REFERRINGPROVIDERID,APPOINTMENTID,"
+        + "CURRENTILLNESSDATE,SERVICEDATE,SUPERVISINGPROVIDERID,STATUS1,STATUS2,STATUSP,"
+        + "OUTSTANDING1,OUTSTANDING2,OUTSTANDINGP,LASTBILLEDDATE1,LASTBILLEDDATE2,LASTBILLEDDATEP,"
+        + "HEALTHCARECLAIMTYPEID1,HEALTHCARECLAIMTYPEID2");
+    claims.write(NEWLINE);
+    claimsTransactions.write("ID,CLAIMID,CHARGEID,PATIENTID,TYPE,AMOUNT,METHOD,FROMDATE,TODATE,"
+        + "PLACEOFSERVICE,PROCEDURECODE,MODIFIER1,MODIFIER2,DIAGNOSISREF1,DIAGNOSISREF2,"
+        + "DIAGNOSISREF3,DIAGNOSISREF4,UNITS,DEPARTMENTID,NOTES,UNITAMOUNT,TRANSFEROUTID,"
+        + "TRANSFERTYPE,PAYMENTS,ADJUSTMENTS,TRANSFERS,OUTSTANDING,APPOINTMENTID,LINENOTE,"
+        + "PATIENTINSURANCEID,FEESCHEDULEID,PROVIDERID,SUPERVISINGPROVIDERID");
+    claimsTransactions.write(NEWLINE);
   }
 
   /**
@@ -1215,6 +1242,41 @@ public class CSVExporter {
     s.append(person.getPayerOwnershipAtTime(Utilities.convertCalendarYearsToTime(startYear)));
     s.append(NEWLINE);
     write(s.toString(), payerTransitions);
+  }
+
+  /**
+   * Write a single claim to claims.csv.
+   *
+   * @param claim The claim to be exported.
+   * @throws IOException if any IO error occurs.
+   */
+  private void claim(Claim claim) throws IOException {
+    // Id,PATIENTID,PROVIDERID,PRIMARYPATIENTINSURANCEID,SECONDARYPATIENTINSURANCEID,
+    // DEPARTMENTID,PATIENTDEPARTMENTID,DIAGNOSIS1,DIAGNOSIS2,DIAGNOSIS3,DIAGNOSIS4,
+    // DIAGNOSIS5,DIAGNOSIS6,DIAGNOSIS7,DIAGNOSIS8,REFERRINGPROVIDERID,APPOINTMENTID,
+    // CURRENTILLNESSDATE,SERVICEDATE,SUPERVISINGPROVIDERID,STATUS1,STATUS2,STATUSP,
+    // OUTSTANDING1,OUTSTANDING2,OUTSTANDINGP,LASTBILLEDDATE1,LASTBILLEDDATE2,LASTBILLEDDATEP,
+    // HEALTHCARECLAIMTYPEID1,HEALTHCARECLAIMTYPEID2
+    StringBuilder s = new StringBuilder();
+    s.append(NEWLINE);
+    write(s.toString(), claims);
+  }
+
+  /**
+   * Write a single claim transaction to claims_transactions.csv.
+   *
+   * @param payer The claim to be exported.
+   * @throws IOException if any IO error occurs.
+   */
+  private void claimTransaction(Claim claim) throws IOException {
+    // ID,CLAIMID,CHARGEID,PATIENTID,TYPE,AMOUNT,METHOD,FROMDATE,TODATE,
+    // PLACEOFSERVICE,PROCEDURECODE,MODIFIER1,MODIFIER2,DIAGNOSISREF1,DIAGNOSISREF2,
+    // DIAGNOSISREF3,DIAGNOSISREF4,UNITS,DEPARTMENTID,NOTES,UNITAMOUNT,TRANSFEROUTID,
+    // TRANSFERTYPE,PAYMENTS,ADJUSTMENTS,TRANSFERS,OUTSTANDING,APPOINTMENTID,LINENOTE,
+    // PATIENTINSURANCEID,FEESCHEDULEID,PROVIDERID,SUPERVISINGPROVIDERID
+    StringBuilder s = new StringBuilder();
+    s.append(NEWLINE);
+    write(s.toString(), claimsTransactions);
   }
 
   /**
