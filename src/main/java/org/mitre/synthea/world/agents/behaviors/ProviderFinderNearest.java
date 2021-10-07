1ba7d08764937834ebf7f08161b61411cd923957
Andy Gregorowicz <andrewg@mitre.org>
2021-10-07 09:17:01 -0400

    Filtering out IHS Hospitals for individuals who are not Native American

--- src/main/java/org/mitre/synthea/world/agents/behaviors/ProviderFinderNearest.java
+++ src/main/java/org/mitre/synthea/world/agents/behaviors/ProviderFinderNearest.java
@@ -1,7 +1,7 @@
 package org.mitre.synthea.world.agents.behaviors;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.mitre.synthea.world.agents.Person;
 import org.mitre.synthea.world.agents.Provider;
@@ -11,37 +11,35 @@ public class ProviderFinderNearest implements IProviderFinder {
 
   @Override
   public Provider find(List<Provider> providers, Person person, EncounterType service, long time) {
-    double distance;
-    double minDistance = Double.MAX_VALUE;
-    List<Provider> options = new ArrayList<Provider>();
+    Stream<Provider> options = providers.stream()
+        // Find providers that accept the person
+        .filter(p -> p.accepts(person, time));
 
-    for (Provider provider : providers) {
-      if (provider.accepts(person, time)
-          && (provider.hasService(service) || service == null)) {
-        if (person.attributes.containsKey("veteran")
-                && !("VA Facility".equals(provider.type))
-                && !(service.equals(
-                        EncounterType.URGENTCARE) || service.equals(EncounterType.EMERGENCY))) {
-          continue;
-        }
-        distance = provider.getLonLat().distance(person.getLonLat());
-        if (distance < minDistance) {
-          options.clear();
-          options.add(provider);
-          minDistance = distance;
-        } else if (distance == minDistance) {
-          options.add(provider);
-        }
+    // Find providers with the requested service, if one is given
+    if (service != null) {
+      options = options.filter(p -> p.hasService(service));
+    }
+
+    // If it's not an emergency
+    if (service == null
+        || !(service.equals(EncounterType.URGENTCARE) || service.equals(EncounterType.EMERGENCY))) {
+      // Filter to only VA Facilities if the person is a veteran
+      if (person.attributes.containsKey("veteran")) {
+        options = options.filter(p -> "VA Facility".equals(p.type));
+      }
+
+      // Filter out IHS facilities if someone is not Native American
+      if (! "NATIVE".equals(person.attributes.get(Person.RACE))) {
+        options = options.filter(p -> ! "IHS Facility".equals(p.type));
       }
     }
+    // Sort by distance
+    options = options.sorted((a, b) -> {
+      double distanceToA = a.getLonLat().distance(person.getLonLat());
+      double distanceToB = b.getLonLat().distance(person.getLonLat());
+      return Double.compare(distanceToA, distanceToB);
+    });
 
-    if (options.isEmpty()) {
-      return null;
-    } else if (options.size() == 1) {
-      return options.get(0);
-    } else {
-      // there are a few equally good options, pick one randomly.
-      return options.get(person.randInt(options.size()));
-    }
+    return options.findFirst().orElse(null);
   }
 }
