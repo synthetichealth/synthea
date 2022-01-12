167417500f8d17e6db66468da4a8c411df2b6ead
Marc Hadley <mhadley@mitre.org>
2022-01-12 15:17:37 -0500

    Add support for remaining dual eligibility codes

--- src/main/java/org/mitre/synthea/export/BB2RIFExporter.java
+++ src/main/java/org/mitre/synthea/export/BB2RIFExporter.java
@@ -48,6 +48,7 @@ import org.mitre.synthea.export.BB2RIFStructure.OUTPATIENT;
 import org.mitre.synthea.export.BB2RIFStructure.PDE;
 import org.mitre.synthea.export.BB2RIFStructure.SNF;
 import org.mitre.synthea.helpers.Config;
+import org.mitre.synthea.helpers.RandomCollection;
 import org.mitre.synthea.helpers.RandomNumberGenerator;
 import org.mitre.synthea.helpers.SimpleCSV;
 import org.mitre.synthea.helpers.Utilities;
@@ -487,25 +488,66 @@ public class BB2RIFExporter {
     return "01";
   }
 
+  // Income level < 0.3
+  private static final RandomCollection<String> incomeBandOneDualCodes = new RandomCollection<>();
+  // 0.3 <= Income level < 0.6
+  private static final RandomCollection<String> incomeBandTwoDualCodes = new RandomCollection<>();
+  // 0.6 <= Income level < 1.0
+  private static final RandomCollection<String> incomeBandThreeDualCodes = new RandomCollection<>();
+
+  static {
+    // Specified Low-Income Medicare Beneficiary (SLMB)-only
+    incomeBandOneDualCodes.add(1.3, "03");
+    // SLMB and full Medicaid coverage, including prescription drugs
+    incomeBandOneDualCodes.add(0.5, "04");
+    // Other dual eligible, but without Medicaid coverage
+    incomeBandOneDualCodes.add(0.007, "09");
+    // Unknown
+    incomeBandOneDualCodes.add(0.05, "99");
+    // Not in code book but present in database
+    incomeBandOneDualCodes.add(0.04, "AA");
+    // Not in code book but present in database
+    incomeBandOneDualCodes.add(0.1, "");
+
+    // QMB and full Medicaid coverage, including prescription drugs
+    incomeBandTwoDualCodes.add(8.2, "02");
+    // Other dual eligible, but without Medicaid coverage
+    incomeBandTwoDualCodes.add(0.007, "09");
+    // Unknown
+    incomeBandTwoDualCodes.add(0.05, "99");
+    // Not in code book but present in database
+    incomeBandTwoDualCodes.add(0.04, "AA");
+    // Not in code book but present in database
+    incomeBandTwoDualCodes.add(0.1, "");
+
+    // Qualified Medicare Beneficiary (QMB)-only
+    incomeBandThreeDualCodes.add(2.2, "01");
+    // Qualifying individuals (QI)
+    incomeBandThreeDualCodes.add(0.8, "06");
+    // Other dual eligible (not QMB, SLMB, QWDI, or QI) with full Medicaid coverage, including
+    // prescription Drugs
+    incomeBandThreeDualCodes.add(2.9, "08");
+    // Other dual eligible, but without Medicaid coverage
+    incomeBandThreeDualCodes.add(0.007, "09");
+    // Unknown
+    incomeBandThreeDualCodes.add(0.05, "99");
+    // Not in code book but present in database
+    incomeBandThreeDualCodes.add(0.04, "AA");
+    // Not in code book but present in database
+    incomeBandThreeDualCodes.add(0.1, "");
+  }
+
   private String getDualEligibilityCode(Person person, int year) {
-    // TBD add support for the following additional codes (%-ages in brackets are observed
+    // TBD add support for the following additional code (%-age in brackets is observed
     // frequency in CMS data):
     // 00 (15.6%) - Not enrolled in Medicare for the month
-    // 04 (0.5%) - SLMB and full Medicaid coverage, including prescription drugs
-    // 06 (0.8%) - Qualifying individuals (QI)
-    // 08 (2.9%) - Other dual eligible (not QMB, SLMB, QWDI, or QI) with full Medicaid coverage,
-    //             including prescription Drugs
-    // 09 (0.007%) - Other dual eligible, but without Medicaid coverage
-    // 99 (0.05%) - Unknown
-    // AA (0.04%) - [Not in Codebook]
-    // [Blank] (0.1%) - [Not in Codebook]
     String partDCostSharingCode = getPartDCostSharingCode(person);
     if (partDCostSharingCode.equals("03")) {
-      return "01"; // (2.2%) Qualified Medicare Beneficiary (QMB)-only
+      return incomeBandThreeDualCodes.next(person);
     } else if (partDCostSharingCode.equals("02")) {
-      return "02"; // (8.2%) QMB and full Medicaid coverage, including prescription drugs
+      return incomeBandTwoDualCodes.next(person);
     } else if (partDCostSharingCode.equals("01")) {
-      return "03"; // (1.3%) Specified Low-Income Medicare Beneficiary (SLMB)-only
+      return incomeBandOneDualCodes.next(person);
     } else if (hasESRD(person, year) || isBlind(person)) {
       return "05"; // (0.001%) Qualified Disabled Working Individual (QDWI)
     } else {
