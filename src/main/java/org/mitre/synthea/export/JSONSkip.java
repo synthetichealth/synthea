111a3727d7b02dedcd5641204e0bb4c78bfd634b
Andy Gregorowicz <andrewg@mitre.org>
2022-01-21 14:10:18 -0500

    Moving inline comment to proper JavaDoc

--- src/main/java/org/mitre/synthea/export/JSONSkip.java
+++ src/main/java/org/mitre/synthea/export/JSONSkip.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This interface is a marker interface indicating that a field should be skipped in the
+ * JSON export.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
 public @interface JSONSkip {
-  // Marker interface indicating that a field should be skipped in the JSON export
 }
