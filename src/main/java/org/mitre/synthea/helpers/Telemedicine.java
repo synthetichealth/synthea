61cc5e010f7ac72004bedebc208145bd16237167
Andy Gregorowicz <andrewg@mitre.org>
2022-03-04 14:20:30 -0500

    WIP: Implementing telemedicine in the Encounter state

---
+++ src/main/java/org/mitre/synthea/helpers/Telemedicine.java
@@ -0,0 +1,41 @@
+package org.mitre.synthea.helpers;
+
+import org.mitre.synthea.world.agents.Person;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/**
+ * A place for helpers related to telemedicine.
+ */
+public class Telemedicine {
+
+  /**
+   * Odds of an encounter being virtual, a.k.a. telemedicine are taken from the McKinsey report:
+   * https://www.mckinsey.com/industries/healthcare-systems-and-services/our-insights/telehealth-a-quarter-trillion-dollar-post-covid-19-reality
+   *
+   * The report claims that since April 2020, telemedicine accounted for 13 - 17% of visits. 15%
+   * was used here for simplicity. It also claims that the current levels of telemedicine are 38
+   * times the prepandemic baseline. That would make the prepandemic baseline 0.4%
+   */
+  public static final double CURRENT_CHANCE = 0.15;
+  public static final double PREPANDEMIC_CHANCE = 0.004;
+
+  public static final long TELEMEDICINE_UPTAKE_DATE = LocalDateTime.of(2020, 4, 1, 12, 0)
+      .toInstant(ZoneOffset.UTC).toEpochMilli();
+
+  /**
+   * Determines whether an encounter should be telemedicine, with different chances before and
+   * after the start of the COVID-19 pandemic.
+   * @param person source of randomness
+   * @param time current time in the simulation
+   * @return true if the encounter should be virtual
+   */
+  public static boolean shouldEncounterBeVirtual(Person person, long time) {
+    if (time < TELEMEDICINE_UPTAKE_DATE) {
+      return person.rand() <= PREPANDEMIC_CHANCE;
+    } else {
+      return person.rand() <= CURRENT_CHANCE;
+    }
+  }
+}
