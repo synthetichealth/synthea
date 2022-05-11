ad187be428e49c86ee709508e8f64bc9e93c52e4
Robi Scalfani <rscalfani@mitre.org>
2022-05-11 17:43:29 -0400

    Added logical operators to attribute insurance eligibility

--- src/main/java/org/mitre/synthea/world/agents/behaviors/planeligibility/QualifyingAttributesEligibility.java
+++ src/main/java/org/mitre/synthea/world/agents/behaviors/planeligibility/QualifyingAttributesEligibility.java
@@ -6,7 +6,10 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.mitre.synthea.helpers.SimpleCSV;
 import org.mitre.synthea.helpers.Utilities;
 import org.mitre.synthea.world.agents.Person;
@@ -16,39 +19,37 @@ import org.mitre.synthea.world.agents.Person;
  */
 public class QualifyingAttributesEligibility implements IPlanEligibility {
 
-  private final List<String> qualifyingAttributes;
+  private final List<AttributeQualifier> qualifyingAttributes;
 
   /**
    * Constructor.
    * @param attributes  The "|" delimited string or file of qualifying attributes.
    */
-  public QualifyingAttributesEligibility(String attributes) {
-    if (attributes.contains("/")) {
+  public QualifyingAttributesEligibility(String attributeInput) {
+    if (attributeInput.contains("/")) {
       // The input is a file, so we have a file that defines the eligible conditions.
-      qualifyingAttributes = buildQualifyingAttributesFile(attributes);
+      qualifyingAttributes = buildQualifyingAttributesFile(attributeInput);
     } else {
       // The input is a set of attributes.
-      qualifyingAttributes = Arrays.asList(attributes.split("\\|"));
+      qualifyingAttributes = new ArrayList<>();
+      for(String attributeExpression : Arrays.asList(attributeInput.split("\\|"))){
+        qualifyingAttributes.add(covertAttributeExpressionToLogic(attributeExpression));
+      }
     }
   }
 
   @Override
   public boolean isPersonEligible(Person person, long time) {
-    boolean attributeEligible = qualifyingAttributes.stream().anyMatch(attribute -> {
-      if (!person.attributes.containsKey(attribute)) {
-        return false;
-      }
-      Object attributeResult = person.attributes.get(attribute);
-      return ((boolean) attributeResult) == true;
-    });
-    return attributeEligible;
+    return qualifyingAttributes.stream().anyMatch(attributeLogic -> 
+      attributeLogic.checkLogic(person)
+    );
   }
 
   /**
    * Builds a list of attributes that would qualify a person for this eligibility type.
    * @return  A list of qualifying attributes.
    */
-  private static List<String> buildQualifyingAttributesFile(String fileName) {
+  private static List<AttributeQualifier> buildQualifyingAttributesFile(String fileName) {
     String resource = null;
     Iterator<? extends Map<String, String>> csv = null;
     try {
@@ -58,13 +59,95 @@ public class QualifyingAttributesEligibility implements IPlanEligibility {
       e.printStackTrace();
     }
 
-    List<String> eligibleAttributes = new ArrayList<String>();
+    List<AttributeQualifier> attributeEligibilities = new ArrayList<AttributeQualifier>();
     while (csv.hasNext()) {
       Map<String, String> row = csv.next();
-      String attribute = row.get("attributes");
-      String[] codes = attribute.split("\\|");
-      eligibleAttributes.addAll(Arrays.asList(codes));
+      String attributeRow = row.get("attributes");
+      String[] attributes = attributeRow.split("\\|");
+      for(String attributeExpression : attributes){
+        AttributeQualifier attributeLogic = covertAttributeExpressionToLogic(attributeExpression);
+        attributeEligibilities.add(attributeLogic);
+      }
     }
-    return eligibleAttributes;
+    return attributeEligibilities;
+  }
+
+  private static AttributeQualifier covertAttributeExpressionToLogic(String attributeExpression) {
+    String regex = " ";
+    String[] splitExpression = attributeExpression.split(regex);
+    if(splitExpression.length != 3){
+      throw new RuntimeException("Invalid attribute expression '" + attributeExpression + "'.");
+    }
+    String attribute = splitExpression[0];
+    String operator = splitExpression[1];
+    String value = splitExpression[2];
+    return new AttributeQualifier(attribute, value, operator);
+  }
+
+  private static class AttributeQualifier {
+
+    @FunctionalInterface
+    private interface AttributeLogic {
+      boolean checkAttributeLogic(Person person);
+    }
+
+    private final AttributeLogic logic;
+
+    AttributeQualifier (String attribute, String value, String intialOperator) {
+      if (intialOperator.equals("=")) {
+        intialOperator = "==";
+      }
+      final String operator = intialOperator;
+      if (NumberUtils.isCreatable(value)) {
+        // If the value is a number, treat it and the attribute as numeric.
+        String[] validOperators = {">=", ">", "<", "<=", "=="};
+        if (Arrays.asList(validOperators).contains(operator)) {
+          logic = 
+            (Person person) -> {
+              Object objResult = person.attributes.get(attribute);
+              if(objResult == null){
+                return false;
+              }
+              Double attributeResult = null;
+              if (objResult instanceof Double) {
+                attributeResult = (Double) objResult;
+              } else if (objResult instanceof Integer) {
+                attributeResult = Double.valueOf((Integer) objResult);
+              } else {
+                throw new RuntimeException("Attribute must be of type integer or double. Recieved type '" + objResult.getClass() + "'.");
+              }
+              return Utilities.compare(attributeResult, Double.parseDouble(value), operator);
+            };
+        } else {
+          throw new RuntimeException("Operator '" + operator + "' is invalid for attribute '" + attribute + "' with value '" + value + "'.");
+        }
+        return;
+      }
+      // Non-numeric logic can only use equality logic.
+      String[] validOperators = {"=="};
+      if (Arrays.asList(validOperators).contains(operator)) {
+        logic = 
+          (Person person) -> {
+            Object attributeResultObj = person.attributes.get(attribute);
+            String attributeResult = "";
+            if (attributeResultObj instanceof Boolean) {
+              attributeResult = String.valueOf((Boolean) attributeResultObj);
+            } else {
+              attributeResult = (String) attributeResultObj;
+              if(attributeResult == null){
+                attributeResult = "false";
+              }
+            }
+            return Utilities.compare(attributeResult, value, operator);
+          };
+      } else {
+        throw new RuntimeException("Operator '" + operator + "' is invalid for attribute '" + attribute + "'.");
+      }
+    }
+
+    boolean checkLogic(Person person) {
+      return this.logic.checkAttributeLogic(person);
+    }
+
   }
 }
