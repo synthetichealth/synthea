b8f134a6593a78f4d403d551ef09dbf0844e34ba
Andy Gregorowicz <andrewg@mitre.org>
2022-11-02 17:22:35 -0400

    Adding an actual test for TelemedicineConfig

--- src/test/java/org/mitre/synthea/world/concepts/TelemedicineConfigTest.java
+++ src/test/java/org/mitre/synthea/world/concepts/TelemedicineConfigTest.java
@@ -1,11 +1,23 @@
 package org.mitre.synthea.world.concepts;
 
 import junit.framework.TestCase;
+import org.apache.commons.math3.util.Pair;
+
+import java.util.List;
 
 public class TelemedicineConfigTest extends TestCase {
 
   public void testFromJSON() {
+    double expectedPreTypicalEmergencyValue = 0.25;
     TelemedicineConfig config = TelemedicineConfig.fromJSON();
-    System.out.println(config.getTelemedicineStartTime());
+    List<Pair<String, Double>> pmf = config.getPreTelemedTypicalEmergency().getPmf();
+    double actualValue = 0;
+    for (int i = 0; i < pmf.size(); i++) {
+      Pair<String, Double> pair = pmf.get(i);
+      if (pair.getFirst().equals(TelemedicineConfig.EMERGENCY)) {
+        actualValue = pair.getSecond();
+      }
+    }
+    assertEquals(expectedPreTypicalEmergencyValue, actualValue);
   }
 }
