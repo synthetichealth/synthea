ba572e73e8545368dd755e335a254ef250d3119c
Andy Gregorowicz <andrewg@mitre.org>
2022-11-30 14:09:10 -0500

    Checkstyle fixes

--- src/test/java/org/mitre/synthea/world/concepts/TelemedicineConfigTest.java
+++ src/test/java/org/mitre/synthea/world/concepts/TelemedicineConfigTest.java
@@ -1,12 +1,16 @@
 package org.mitre.synthea.world.concepts;
 
-import junit.framework.TestCase;
-import org.apache.commons.math3.util.Pair;
-
 import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.math3.util.Pair;
 
 public class TelemedicineConfigTest extends TestCase {
 
+  /**
+   * Test reading in a config file from JSON.
+   */
   public void testFromJSON() {
     double expectedPreTypicalEmergencyValue = 0.25;
     TelemedicineConfig config = TelemedicineConfig.fromJSON();
