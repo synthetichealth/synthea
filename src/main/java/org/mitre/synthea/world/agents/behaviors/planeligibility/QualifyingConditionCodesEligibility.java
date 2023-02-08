b956538a64cc77b21d2184ab9eb3c98de9a1766d
Jason Walonoski <jwalonoski@mitre.org>
2023-02-08 15:11:40 -0500

    Remove unused import.

--- src/main/java/org/mitre/synthea/world/agents/behaviors/planeligibility/QualifyingConditionCodesEligibility.java
+++ src/main/java/org/mitre/synthea/world/agents/behaviors/planeligibility/QualifyingConditionCodesEligibility.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import org.mitre.synthea.helpers.SimpleCSV;
 import org.mitre.synthea.helpers.Utilities;
