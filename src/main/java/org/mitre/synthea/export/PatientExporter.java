abf8ac78a5839fb5187c2dee545f4df3493271ca
Dylan Hall <dehall@mitre.org>
2023-08-17 11:38:13 -0400

    cleanup

--- src/main/java/org/mitre/synthea/export/PatientExporter.java
+++ src/main/java/org/mitre/synthea/export/PatientExporter.java
@@ -4,13 +4,12 @@ import org.mitre.synthea.export.Exporter.ExporterRuntimeOptions;
 import org.mitre.synthea.world.agents.Person;
 
 /**
- * 
- *
+ * This interface defines an exporter that will be run for every patient
+ * in a simulation.
  */
 public interface PatientExporter {
-
   /**
-   * 
+   * Export the given Person object.
    * @param person   Patient to export
    * @param stopTime Time at which the simulation stopped
    * @param options Runtime exporter options
