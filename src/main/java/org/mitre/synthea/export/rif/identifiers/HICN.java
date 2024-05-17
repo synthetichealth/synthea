bd4c558677b7aa3f1d3d66da6016e6ea3196b086
Marc Hadley <mhadley@mitre.org>
2024-05-17 14:08:29 -0400

    Fix checkstyle warnings

--- src/main/java/org/mitre/synthea/export/rif/identifiers/HICN.java
+++ src/main/java/org/mitre/synthea/export/rif/identifiers/HICN.java
@@ -12,10 +12,10 @@ public class HICN extends FixedLengthIdentifier {
 
   private static final char[] START = {'T'};
   private static final char[] END = {'A'};
-  private static final char[][] FAKE_HICN_FORMAT = {START, NUMERIC, NUMERIC, NUMERIC, NUMERIC, NUMERIC,
-    NUMERIC, NUMERIC, NUMERIC, END};
-  private static final char[][] REAL_HICN_FORMAT = {NUMERIC, NUMERIC, NUMERIC, NUMERIC, NUMERIC, NUMERIC,
-    NUMERIC, NUMERIC, NUMERIC, END};
+  private static final char[][] FAKE_HICN_FORMAT = {START, NUMERIC, NUMERIC,
+    NUMERIC, NUMERIC, NUMERIC, NUMERIC, NUMERIC, NUMERIC, END};
+  private static final char[][] REAL_HICN_FORMAT = {NUMERIC, NUMERIC, NUMERIC,
+    NUMERIC, NUMERIC, NUMERIC, NUMERIC, NUMERIC, NUMERIC, END};
   public static final long MIN_HICN = 0;
   public static final long MAX_FAKE_HICN = maxValue(FAKE_HICN_FORMAT);
   public static final long MAX_REAL_HICN = maxValue(REAL_HICN_FORMAT);
@@ -30,6 +30,12 @@ public class HICN extends FixedLengthIdentifier {
     this.fake = fake;
   }
 
+  /**
+   * Parse a HICN from a string.
+   * @param str the string to parse
+   * @return the HICN
+   * @throws IllegalArgumentException if st isn't a valid HICN
+   */
   public static HICN parse(String str) {
     if (str.charAt(0) == 'T' || str.charAt(0) == 't') {
       return parse(str, true);
@@ -39,7 +45,8 @@ public class HICN extends FixedLengthIdentifier {
   }
 
   private static HICN parse(String str, boolean fake) {
-    return new HICN(parse(str, fake ? FAKE_HICN_FORMAT : REAL_HICN_FORMAT), fake);
+    return new HICN(parse(str,
+            fake ? FAKE_HICN_FORMAT : REAL_HICN_FORMAT), fake);
   }
 
   @Override
